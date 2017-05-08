979717e83a1134fef99b0c749504395c94b2ed04
sri49 <sriharsha.poosa@gmail.com>
2017-05-08 13:48:40 -0400

    Added transalations for insure module

--- spec/views/insured/employee_roles/privacy.html.erb_spec.rb
+++ spec/views/insured/employee_roles/privacy.html.erb_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "insured/employee_roles/privacy.html.erb" do
   let(:person) {FactoryGirl.create(:person)}
+  let(:your_information) { FactoryGirl.create(:translation, key: "en.insured.your_information", value: '"Your Information"') }
+  let(:continue) { FactoryGirl.create(:translation, key: "en.continue", value: '"Continue"') }
 
   before :each do
     assign(:person, person)
@@ -9,9 +11,9 @@ RSpec.describe "insured/employee_roles/privacy.html.erb" do
   end
 
   it "should display the employee privacy message" do
-    expect(rendered).to have_selector('h1', text: 'Your Information')
-    expect(rendered).to have_selector("strong", text: 'Please read the information below and click the')
-    expect(rendered).to match(/Your answers on this application will only be/i)
-    expect(rendered).to have_selector('.btn', text: 'CONTINUE')
+    expect(rendered).to have_selector('h1', text: "#{l10n('insured.your_information')}")
+    # expect(rendered).to have_selector("strong", text: 'Please read the information below and click the')
+    # expect(rendered).to match(/Your answers on this application will only be/i)
+    expect(rendered).to have_selector('.btn', text: "#{l10n("continue").to_s.upcase}")
   end
 end
