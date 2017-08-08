65f925257e922c33c895a3fa58fc3b8d2a27da49
harsha280 <harsha.vskr@gmail.com>
2017-08-08 11:27:30 -0400

    Refs 17115 : Fixed Cucumber

--- features/employers/step_definitions/employer_attestation_steps.rb
+++ features/employers/step_definitions/employer_attestation_steps.rb
@@ -4,7 +4,7 @@ Then (/^Employer Staff should see dialog with Attestation warning$/) do
   expect(page).to have_content('Cancel')
 end
 
-Then (/^Employer Staff should not see force publish $/) do
+Then (/^Employer Staff should not see force publish$/) do
   wait_for_ajax(3,2)
   expect(page).not_to have_content('Force Publish')
   expect(page).to have_content('Go Back')
