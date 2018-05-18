e69cc0dfd2d9608c9fa65e0701dc5b38b02d5e89
saip7795 <saip7795@gmail.com>
2018-05-18 09:18:02 -0400

    Ref24025: adding a condition for not printing the puts statement while running rspec

--- app/data_migrations/cancel_plan_years_group.rb
+++ app/data_migrations/cancel_plan_years_group.rb
@@ -8,23 +8,23 @@ class CancelPlanYearsGroup < MongoidMigrationTask
       date =(Date.strptime(row['start_on'],'%m/%d/%Y').to_date)
       aasm_state = row['aasm_state']
       if !EmployerProfile.find_by_fein(row['FEIN']).present?
-        puts "employer not found #{row['FEIN']}" 
+        puts "employer not found #{row['FEIN']}"  unless Rails.env.test?
       else  
         plan_year=EmployerProfile.find_by_fein(row['FEIN']).plan_years.where(start_on: date, aasm_state: aasm_state).first
         if plan_year.present?
             if ["application_ineligible","renewing_application_ineligible"].include? plan_year.aasm_state
               enrollments = all_enrollments(plan_year.benefit_groups)
               enrollments.each { |enr| enr.cancel_coverage! if enr.may_cancel_coverage? }
-              puts "canceled enrollments for ineligible plan year"
+              puts "canceled enrollments for ineligible plan year" unless Rails.env.test?
               plan_year.revert_application! if plan_year.may_revert_application?
               plan_year.cancel! if plan_year.may_cancel?
-              puts "canceled ineligible plan year for #{row['FEIN']}"
+              puts "canceled ineligible plan year for #{row['FEIN']}" unless Rails.env.test?
             else
               system("rake migrations:cancel_plan_year feins='#{row['FEIN']}' plan_year_state='#{row['aasm_state']}'")
               puts "Plan Year Cancelled for #{row['FEIN']}" unless Rails.env.test?
             end
         else
-          puts "Plan Year not found for ER #{row['FEIN']} with the #{row['start_on']} and #{row['aasm_state']}"
+          puts "Plan Year not found for ER #{row['FEIN']} with the #{row['start_on']} and #{row['aasm_state']}" unless Rails.env.test?
         end  
       end
     end
