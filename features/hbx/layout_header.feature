48b1c02d4c3c9a2cf5195cf8907acbe07b1875cb
harsha280 <harsha.vskr@gmail.com>
2018-06-04 15:15:03 -0400

    Refs 17871 Logo Fix For MA and DC

--- features/hbx/layout_header.feature
+++ features/hbx/layout_header.feature
@@ -3,5 +3,5 @@ Feature: HBX Admin should be able to click the MA logo link
   Scenario: HBX Admin is on Landing Page
    Given a Hbx admin with read and write permissions exists
    When Hbx Admin logs on to the Hbx Portal
-   And the Hbx Admin clicks on the Ma logo link
-   Then it should redirect to Ma Health connector website
+   And the Hbx Admin clicks on the logo link
+   Then it should redirect to Health Care Website
--- features/hbx/step_definitions/layout_header_steps.rb
+++ features/hbx/step_definitions/layout_header_steps.rb
@@ -1,8 +1,8 @@
-When(/^the Hbx Admin clicks on the Ma logo link$/) do
+When(/^the Hbx Admin clicks on the logo link$/) do
   find(:xpath, '//*[@id="ma_logo"]/img').trigger('click')
 end
 
-Then(/^it should redirect to Ma Health connector website$/) do
+Then(/^it should redirect to Health Care Website$/) do
   wait_for_ajax(3,2)
-  expect(page).to have_content("Health Connector for Business")
+  expect(page).to have_content("#{Settings.site.short_name}")
 end
