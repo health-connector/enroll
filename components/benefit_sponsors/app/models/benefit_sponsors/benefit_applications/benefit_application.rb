40e81b1bfc171f89a4eba8ab8c9021466486a1a4
Zoheb Nensey <zoheb.nensey@gmail.com>
2018-06-28 00:43:14 -0400

    Refs #25579 add force publish event to state machine

--- components/benefit_sponsors/app/models/benefit_sponsors/benefit_applications/benefit_application.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/benefit_applications/benefit_application.rb
@@ -283,6 +283,12 @@ module BenefitSponsors
       super(open_enrollment_range) unless open_enrollment_range.blank?
     end
 
+    def adjust_open_enrollment_date
+      if TimeKeeper.date_of_record > open_enrollment_start_on && TimeKeeper.date_of_record < open_enrollment_end_on
+        open_enrollment_period=((TimeKeeper.date_of_record.to_time.utc.beginning_of_day)..open_enrollment_end_on)
+      end
+    end
+
     def find_census_employees
       return @census_employees if defined? @census_employees
       @census_employees ||= CensusEmployee.benefit_application_assigned(self)
@@ -539,6 +545,23 @@ module BenefitSponsors
       self
     end
 
+    def is_application_invalid?
+      self.valid?
+    end
+
+    def is_application_eligible?
+      return false if no_documents_uploaded?
+      return false unless benefit_sponsorship.has_primary_office_address? && benefit_sponsorship.profile.is_primary_office_local?
+      return false if aasm_state == 'enrollment_ineligible'
+      return false if fte_count < 1 || fte_count > Settings.aca.shop_market.small_market_employee_count_maximum
+      true
+    end
+
+    def accept_application
+      adjust_open_enrollment_date
+      transition_success = benefit_sponsorship.initial_application_approved! if benefit_sponsorship.may_approve_initial_application?
+    end
+
     class << self
 
       def find(id)
@@ -655,6 +678,13 @@ module BenefitSponsors
           to:     :active
         transitions from:   [:denied, :enrollment_closed, :enrollment_ineligible],
           to:     :expired
+      end
+
+      event :force_publish do 
+        transitions from: :draft, to: :draft, guard: :is_application_invalid?
+        transitions from: :imported, to: :imported, guard: :is_application_invalid?, :after => [:accept_application, :refresh_recorded_rating_area, :refresh_recorded_sic_code]
+        transitions from: [:draft, :imported], to: :enrollment_open, guard: :is_application_eligible?
+        transitions from :draft, to: :pending
       end
 
       event :expire do
