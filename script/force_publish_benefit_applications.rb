c6f3ea1c67d6c4bc88c083aa6e20527275a46ddf
Zoheb Nensey <zoheb.nensey@gmail.com>
2018-06-29 12:23:23 -0400

    Refs #25579 update benefit applications code to safely simulate provisional renewals

--- script/force_publish_benefit_applications.rb
+++ script/force_publish_benefit_applications.rb
@@ -10,16 +10,24 @@ elsif date.day <= 15
   window = (window_start..window_end)
 end
 
+def select_benefit_application(benefit_sponsorship)
+  if benefit_sponsorship.renewing_submitted_benefit_application.present?
+    return benefit_sponsorship.renewing_submitted_benefit_application
+  else
+    return benefit_sponsorship.renewal_benefit_application
+  end
+end
+
 start_on_date = window.end.next_month.beginning_of_month
 
 benefit_sponsorships = BenefitSponsors::BenefitSponsorships::BenefitSponsorship.where("benefit_applications.effective_period.min" => start_on_date.to_time.utc.beginning_of_day)
 
 benefit_applications = []
-benefit_sponsorships.each{|bs| benefit_applications << bs.benefit_applications.detect{|ba| ba.effective_period.min == start_on_date}}
+benefit_sponsorships.each{|bs| benefit_applications << select_benefit_application(bs)}
 
 benefit_applications.each do |ba|
   begin
-    ba.simulate_provisional_renewal!
+    ba.simulate_provisional_renewal! if may_simulate_provisional_renewal?
   rescue Exception=>e
     puts "Could not force publish #{ba.benefit_sponsorship.organization.legal_name} because of #{e.inspect}"
     next
