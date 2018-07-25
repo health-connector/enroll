9165f12c2310a6ccfd67d8620aa85dbb287dc522
Battula <vrbattula@outlook.com>
2018-07-25 12:26:50 -0400

    refs #27483 Updated query to use predecessor.

--- components/notifier/app/models/notifier/builders/benefit_application.rb
+++ components/notifier/app/models/notifier/builders/benefit_application.rb
@@ -219,11 +219,7 @@ module Notifier
       benefit_application = load_benefit_application
       if benefit_application.present?
         if benefit_application.is_renewing? && (benefit_application.pending? || benefit_application.is_submitted? || benefit_application.canceled? || benefit_application.draft? || benefit_application.enrollment_ineligible?)
-          if employer_profile.active_benefit_sponsorship.is_conversion?
-            @current_benefit_application = employer_profile.active_benefit_sponsorship.benefit_applications.detect{|ba| (ba.imported? || ba.is_submitted?) && ba.end_on == benefit_application.start_on.to_date.prev_day}
-          else
-            @current_benefit_application = employer_profile.active_benefit_sponsorship.benefit_applications.detect{|ba| ba.is_submitted? && ba.end_on == benefit_application.start_on.to_date.prev_day}
-          end
+          @current_benefit_application = benefit_application.predecessor
         else
           @current_benefit_application = benefit_application
         end
