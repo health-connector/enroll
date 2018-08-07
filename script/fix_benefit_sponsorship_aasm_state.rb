f1af8e9eb7bb4fd560531a93e6f26fe12c1acebf
saimekala07 <saimekala07@gmail.com>
2018-08-07 13:57:11 -0400

    refs #28211 updating condition..

--- script/fix_benefit_sponsorship_aasm_state.rb
+++ script/fix_benefit_sponsorship_aasm_state.rb
@@ -1,5 +1,5 @@
 BenefitSponsors::BenefitSponsorships::BenefitSponsorship.all.each do |benefit_sponsorship|
-  if benefit_sponsorship.active_benefit_application.present? && benefit_sponsorship.aasm_state == :applicant
+  if (benefit_sponsorship.renewal_benefit_application.present? || benefit_sponsorship.active_benefit_application.present?) && benefit_sponsorship.aasm_state != :active
     benefit_sponsorship.workflow_state_transitions << WorkflowStateTransition.new(from_state: benefit_sponsorship.aasm_state, to_state: :active)
     benefit_sponsorship.update_attributes(aasm_state: :active)
   end
