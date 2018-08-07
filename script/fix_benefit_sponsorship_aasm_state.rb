9baf821d34cc3a1efe89a7c2ce469870fb6601e6
saimekala07 <saimekala07@gmail.com>
2018-08-07 13:37:34 -0400

    refs #28211 Adding script to update benefit_sponsorship aasm state in DB.

---
+++ script/fix_benefit_sponsorship_aasm_state.rb
@@ -0,0 +1,6 @@
+BenefitSponsors::BenefitSponsorships::BenefitSponsorship.all.each do |benefit_sponsorship|
+  if benefit_sponsorship.active_benefit_application.present? && benefit_sponsorship.aasm_state == :applicant
+    benefit_sponsorship.workflow_state_transitions << WorkflowStateTransition.new(from_state: benefit_sponsorship.aasm_state, to_state: :active)
+    benefit_sponsorship.update_attributes(aasm_state: :active)
+  end
+end
