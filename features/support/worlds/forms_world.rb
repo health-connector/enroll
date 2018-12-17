3936cab4028d73c2799c87a280c7c7d4c4d8c904
202chaz <chaz.haskins@gmail.com>
2018-12-17 06:12:08 -0500

    Form feature update

--- features/support/worlds/forms_world.rb
+++ features/support/worlds/forms_world.rb
@@ -1,11 +1,11 @@
 module FormsWorld
 
-  def fill_in_employer_registration_form
+  def fill_in_employer_registration_form_as_admin
     fill_in 'agency_organization_legal_name', with: registering_employer.legal_name
     fill_in 'agency_organization_dba', with: registering_employer.dba
     fill_in 'agency_organization_fein', with: registering_employer.fein
     select 'Tax Exempt Organization', from: 'agency_organization_entity_kind'
-    #select "0111", from: "agency_organization_profile_attributes_sic_code"
+    #select "Wheat - 0111", from: "agency_organization_profile_attributes_sic_code"
     fill_in 'inputAddress1', with: registering_employer.employer_profile.office_locations.first.address.address_1
     fill_in 'agency_organization_profile_attributes_office_locations_attributes_0_address_attributes_city', with: registering_employer.employer_profile.office_locations.first.address.city
     select registering_employer.employer_profile.office_locations.first.address.state, from: 'inputState'
@@ -19,5 +19,8 @@ end
 World(FormsWorld)
 
 Given(/^all required fields have valid inputs on the Employer Registration Form$/) do
-  fill_in_employer_registration_form
+  if @admin
+    fill_in_employer_registration_form_as_admin
+  else
+  end
 end
