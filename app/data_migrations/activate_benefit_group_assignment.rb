b72e8281ef32fd002b05ee7b8c557be076bca011
saikumar9 <saikumarkotagiri9@gmail.com>
2019-02-05 14:16:50 -0500

    Refs #39674 Updated rake which activates BGA of a CE

--- app/data_migrations/activate_benefit_group_assignment.rb
+++ app/data_migrations/activate_benefit_group_assignment.rb
@@ -3,16 +3,17 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 class ActivateBenefitGroupAssignment < MongoidMigrationTask
   def migrate
     begin
-      census_employee = CensusEmployee.by_ssn(ENV['ce_ssn']).first
+      bga_id = ENV['bga_id']
+      census_employee = CensusEmployee.by_benefit_group_assignment_ids([bga_id]).first
+
       if census_employee.nil?
-        puts "No census employee was found with given ssn"
+        puts "No census employee was found with given benefit group assignment id" unless Rails.env.test?
       else
-        benefit_group_assignment=census_employee.benefit_group_assignments.where(id:ENV['bga_id']).first
-        benefit_group_assignment.make_active
+        benefit_group_assignment = census_employee.benefit_group_assignments.find(bga_id)
+        benefit_group_assignment.make_active if benefit_group_assignment
       end
     rescue => e
-      e.message
+      puts "Unable to Activate Benefit Group Assignment because of Error: #{e}, Backtrace: #{e.backtrace}" unless Rails.env.test?
     end
-
   end
 end
