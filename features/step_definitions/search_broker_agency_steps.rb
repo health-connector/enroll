ddb92c1a956f57d15b496f7651233b27f67cb862
zasman <zanesandin@gmail.com>
2019-03-18 14:32:01 -0400

    Fixes syntax error

--- features/step_definitions/search_broker_agency_steps.rb
+++ features/step_definitions/search_broker_agency_steps.rb
@@ -15,7 +15,7 @@ Then(/^he should see the one result with the agency name$/) do
 end
 
 
-And(/^Hbx Admin clicks on (*?)$/) do |tab_name|
+And(/^Hbx Admin clicks on (.*?)$/) do |tab_name|
   target_dropdown = page.all('a').detect { |a| a.text == tab_name }
   target_dropdown.click
   wait_for_ajax
