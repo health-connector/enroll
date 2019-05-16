6718ec0e3768aef8156936d5155dcf33b4738c81
Trey Evans <tevans@Just-Plain-Awful.local>
2019-05-15 21:44:33 -0400

    Adjust permissions to reduce read-only admin abilities.

--- app/controllers/exchanges/security_questions_controller.rb
+++ app/controllers/exchanges/security_questions_controller.rb
@@ -10,6 +10,7 @@ class Exchanges::SecurityQuestionsController < ApplicationController
   end
 
   def create
+    authorize HbxProfile, :modify_admin_tabs?
     @question = SecurityQuestion.new(security_question_params)
     # binding.pry
     if @question.save
@@ -20,10 +21,12 @@ class Exchanges::SecurityQuestionsController < ApplicationController
   end
 
   def edit
+    authorize HbxProfile, :modify_admin_tabs?
     @question = SecurityQuestion.find(params[:id])
   end
 
   def update
+    authorize HbxProfile, :modify_admin_tabs?
     @question = SecurityQuestion.find(params[:id])
     if @question.safe_to_edit_or_delete? && @question.update_attributes(security_question_params)
       redirect_to exchanges_security_questions_path, notice: 'Question was updated successfully'
@@ -33,6 +36,7 @@ class Exchanges::SecurityQuestionsController < ApplicationController
   end
 
   def destroy
+    authorize HbxProfile, :modify_admin_tabs?
     @question = SecurityQuestion.find(params[:id])
     if @question.safe_to_edit_or_delete?
       @question.destroy
