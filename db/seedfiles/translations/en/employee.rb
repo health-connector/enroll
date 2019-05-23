59dadb3c44594a76f28a38b38213917543daa7f6
sri49 <sriharsha.poosa@gmail.com>
2019-05-23 15:53:49 -0400

    Refs #42013. EE views translations

---
+++ db/seedfiles/translations/en/employee.rb
@@ -0,0 +1,13 @@
+EMPLOYEE_TRANSLATIONS = {
+	'en.employee.ee_prohibited' => 'prohibited this employee from being saved:',
+	'en.address' => 'Address',
+	'en.employees.new.welcome_sign_up' => "<span>Welcome.</span> Let's get you signed up for health care. Lorem ipsum dolot sit amet. Ut enim ad minim vineam, quis nostrud exarciation ullamco laboris nisi ut aliquip ex ea commondo consequat. Lorem ipsum dolor sit amet. Ut enim ad dolor sit",
+	'en.employees.auto_fill_info' => 'We may be able to auto-fill your information with data from our records.',
+	'en.employees.pi_secure' => 'Your personal information is secure.',
+	'en.save_exit' => 'Save & Exit',
+	'en.thanks' => 'Thanks',
+	'en.employees.match_error' => 'We were not able to find your information. Double check your social security number and birthdate',
+	'en.try_again' => 'Try Again',
+	'en.auto_fill' => 'Autofill'
+
+}
