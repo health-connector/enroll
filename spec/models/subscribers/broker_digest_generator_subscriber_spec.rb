1f5a9bfec294b4a9fe58531863321496994d3d6b
Trey Evans <tevans@justplainawful.lan>
2020-05-19 16:53:38 -0400

    More specs.

--- spec/models/subscribers/broker_digest_generator_subscriber_spec.rb
+++ spec/models/subscribers/broker_digest_generator_subscriber_spec.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 require "rails_helper"
+require "base64"
 
-describe Subscribers::BrokerDigestGeneratorSubscriber, "given active broker exists" do
+describe Subscribers::BrokerDigestGeneratorSubscriber, "given active broker exists", dbclean: :after_each do
   let(:broker_agency_organization) { FactoryGirl.create(:benefit_sponsors_organizations_general_organization,:with_site,:with_broker_agency_profile)}
   let!(:broker_agency_profile) { broker_agency_organization.broker_agency_profile }
   let!(:person_broker) {FactoryGirl.create(:person,:with_work_email, :with_work_phone)}
@@ -10,7 +11,7 @@ describe Subscribers::BrokerDigestGeneratorSubscriber, "given active broker exis
 
   subject { Subscribers::BrokerDigestGeneratorSubscriber.new }
 
-  describe "should generate broker digest" do
+  describe "generating a broker digest" do
 
     it "should notify broker digest published event" do
       expect(subject).to receive(:notify) do |recipient, hash|
@@ -20,6 +21,58 @@ describe Subscribers::BrokerDigestGeneratorSubscriber, "given active broker exis
       end
       return_status = subject.work_with_params(nil, nil, {})
       expect(return_status).to eq :ack
+    end
+
+    it "should be valid zip file, with 2 entries" do
+      body_result = nil
+      expect(subject).to receive(:notify) do |recipient, hash|
+        body_result = hash[:body]
+      end
+      subject.work_with_params(nil, nil, {})
+      binary = Base64.decode64(body_result)
+      zip_stream = StringIO.new(binary)
+      entry_count = 0
+      Zip::InputStream.open(zip_stream) do |zstream|
+        while (entry = zstream.get_next_entry)
+          entry_count = entry_count + 1
+        end
+      end
+      # We expect one for the directory, and one for the file.
+      expect(entry_count).to eq 2
+    end
+
+    it "is a zip file, with an entry for the XML directory" do
+      body_result = nil
+      expect(subject).to receive(:notify) do |recipient, hash|
+        body_result = hash[:body]
+      end
+      subject.work_with_params(nil, nil, {})
+      binary = Base64.decode64(body_result)
+      zip_stream = StringIO.new(binary)
+      entry_names = []
+      Zip::InputStream.open(zip_stream) do |zstream|
+        while (entry = zstream.get_next_entry)
+          entry_names << entry.name
+        end
+      end
+      expect(entry_names).to include("broker_xmls/")
+    end
+
+    it "is a zip file, with an entry for the single broker XML, by NPN" do
+      body_result = nil
+      expect(subject).to receive(:notify) do |recipient, hash|
+        body_result = hash[:body]
+      end
+      subject.work_with_params(nil, nil, {})
+      binary = Base64.decode64(body_result)
+      zip_stream = StringIO.new(binary)
+      entry_names = []
+      Zip::InputStream.open(zip_stream) do |zstream|
+        while (entry = zstream.get_next_entry)
+          entry_names << entry.name
+        end
+      end
+      expect(entry_names).to include("broker_xmls/#{broker.npn}.xml")
     end
   end
 end
