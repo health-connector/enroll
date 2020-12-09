25769c8d52d2e865b3213a24f09276a0be099b72
Battula <vrbattula@outlook.com>
2020-12-09 17:41:00 -0500

    refs #91569 Fix issues with terminated benefit application

--- components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/employers/employer_profiles_controller.rb
+++ components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/employers/employer_profiles_controller.rb
@@ -191,6 +191,9 @@ module BenefitSponsors
                 off_cycle: true
               }
             )
+            data_table_params.merge!(
+              current_py_terminated: true
+            ) if @employer_profile.current_benefit_application&.terminated?
           end
 
           data_table_params.merge!({
