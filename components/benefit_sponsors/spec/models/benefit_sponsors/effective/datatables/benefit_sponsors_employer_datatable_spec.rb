8d6f1d432ebc6c4b5a2e0138b218cb82a9749a86
Zane Sandin <zanesandin@gmail.com>
2021-06-03 16:55:17 -0400

    REFS Maintenance - sets up more specs.

--- components/benefit_sponsors/spec/models/benefit_sponsors/effective/datatables/benefit_sponsors_employer_datatable_spec.rb
+++ components/benefit_sponsors/spec/models/benefit_sponsors/effective/datatables/benefit_sponsors_employer_datatable_spec.rb
@@ -43,49 +43,85 @@ module Effective
 
       context "scopes" do
         context "benefit application" do
-          before do
-          end
+          context "applicant" do
+            before do
+              # applicant_attributes = {"employers" => "benefit_sponsorship_applicant"}.with_indifferent_access
+            end
 
+            xit "should return a valid datatable only including applicant benefit applications" do
+
+            end
+
+          end
           context "enrolled" do
+            before do
+              # enrolled_attributess = {"employers" => "benefit_application_enrolled"}.with_indifferent_access
+            end
+
             xit "should return a valid datatable only including enrolled benefit applications" do
 
             end
           end
+
+          context "enrolling" do
+            before do
+              # enrolling_attributes = {"employers" => "benefit_application_enrolling"}.with_indifferent_access
+
+            end
+
+            xit "should  a valid datatable only including enrolling benefit applications" do
+
+            end
+
+          end
         end
         context "employer attestations" do
-          let(:random_org) do
-            BenefitSponsors::Organizations::Organization.all.detect do |org|
-              org.employer_profile.present? && org.employer_profile.employer_attestation.present?
+          context "all" do
+            let(:random_org) do
+              BenefitSponsors::Organizations::Organization.all.detect do |org|
+                org.employer_profile.present? && org.employer_profile.employer_attestation.present?
+              end
+            end
+            let(:random_org_profile){ random_org.employer_profile }
+            let(:non_attestation_org) do
+              BenefitSponsors::Organizations::Organization.all.detect do |org|
+                org.employer_profile.present? &&
+                  org.employer_profile.employer_attestation.present? &&
+                  org.benefit_sponsorships.present?
+              end
+            end
+            let(:non_attestation_profile) { non_attestation_org.employer_profile }
+
+            before do
+              # Submitted is an employer attestation status
+              random_org_profile.employer_attestation.update_attributes(aasm_state: "submitted")
+              # Destroy to make sure it doens't appear
+              non_attestation_profile.employer_attestation.destroy
+              employer_attestation_attributes = {
+                "custom_attributes" => nil,
+                "employers" => "employer_attestations"
+              }.with_indifferent_access
+              @datatable = ::Effective::Datatables::BenefitSponsorsEmployerDatatable.new
+              @datatable.attributes = employer_attestation_attributes
+            end
+
+            it "should return a valid datatable only including employer attestation benefit sponsorship" do
+              random_org_profile_benefit_sponsorship_id = random_org_profile.benefit_sponsorships.last.id.to_s
+              expect(@datatable.collection.where(_id: random_org_profile_benefit_sponsorship_id).first.present?).to eq(true)
+              non_attestation_profile_benefit_sponsorship_id = non_attestation_profile.benefit_sponsorships.last.id.to_s
+              expect(@datatable.collection.where(_id: non_attestation_profile_benefit_sponsorship_id).first.present?).to eq(true)
             end
           end
-          let(:random_org_profile){ random_org.employer_profile }
-          let(:non_attestation_org) do
-            BenefitSponsors::Organizations::Organization.all.detect do |org|
-              org.employer_profile.present? &&
-                org.employer_profile.employer_attestation.present? &&
-                org.benefit_sponsorships.present?
+          context "denied" do
+            before do
+              # denied_attributes = {
+              #  "employers" => "employer_attestations",
+              #  "attestations" => "denied"
+              # }.with_indifferent_access
             end
-          end
-          let(:non_attestation_profile) { non_attestation_org.employer_profile }
+            xit "should return a valid datatable only including denid employer attestation benefit sponsorships" do
 
-          before do
-            # Submitted is an employer attestation status
-            random_org_profile.employer_attestation.update_attributes(aasm_state: "submitted")
-            # Destroy to make sure it doens't appear
-            non_attestation_profile.employer_attestation.destroy
-            employer_attestation_attributes = {
-              "custom_attributes" => nil,
-              "employers" => "employer_attestations"
-            }.with_indifferent_access
-            @datatable = ::Effective::Datatables::BenefitSponsorsEmployerDatatable.new
-            @datatable.attributes = employer_attestation_attributes
-          end
-
-          it "should return a valid datatable only including employer attestation benefit sponsorship" do
-            random_org_profile_benefit_sponsorship_id = random_org_profile.benefit_sponsorships.last.id.to_s
-            expect(@datatable.collection.where(_id: random_org_profile_benefit_sponsorship_id).first.present?).to eq(true)
-            non_attestation_profile_benefit_sponsorship_id = non_attestation_profile.benefit_sponsorships.last.id.to_s
-            expect(@datatable.collection.where(_id: non_attestation_profile_benefit_sponsorship_id).first.present?).to eq(true)
+            end
           end
         end
       end
