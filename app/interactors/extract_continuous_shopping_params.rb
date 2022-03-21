4aef4b9c79f1a8d8f17d316a657ca231a32c7ea6
Vishal Kalletla <vishuk1201@gmail.com>
2022-03-21 17:28:43 -0400

    more code for continuous coverage page

--- app/interactors/extract_continuous_shopping_params.rb
+++ app/interactors/extract_continuous_shopping_params.rb
@@ -8,10 +8,11 @@ class ExtractContinuousShoppingParams
   end
 
   def call
-    context.employee_role = hbx_enrollment.employee_role
-    context.market_kind = hbx_enrollment.kind
+    context.employee_role_id = hbx_enrollment.employee_role.id
+    context.person_id = hbx_enrollment.employee_role.person.id
+    context.market_kind = "shop" # This is considered as param from UI, Passing this as default shop
     context.enrollment_kind = hbx_enrollment.enrollment_kind
-    context.effective_on = hbx_enrollment.effective_on
+    context.new_effective_on = hbx_enrollment.effective_on
   rescue StandardError => _e
     context.fail!(message: "invalid cart enrollment id")
   end
