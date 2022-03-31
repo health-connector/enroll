20c3059c625963233b61c1ad1058fcabc5b7dfba
Vishal Kalletla <vishuk1201@gmail.com>
2022-03-31 10:16:06 -0400

    refactor interactors and add specs

--- app/interactors/fetch_shop_benefit.rb
+++ app/interactors/fetch_shop_benefit.rb
@@ -6,14 +6,28 @@ class FetchShopBenefit
   def call
     return unless context.market_kind == 'shop' || context.market_kind == 'fehb'
 
-    assigned_benefit_package = context.employee_role.benefit_package(qle: context.qle, shop_under_current: context.shop_under_current, shop_under_future: context.shop_under_future) if context.employee_role.present?
-
-    possible_benefit_package = context.previous_hbx_enrollment.sponsored_benefit_package if context.change_plan.present? && context.previous_hbx_enrollment.present?
-
     context.benefit_group = if possible_benefit_package.present? && assigned_benefit_package&.start_on != possible_benefit_package.start_on
                               possible_benefit_package
                             else
                               assigned_benefit_package
                             end
+  end
+
+  def assigned_benefit_package
+    return unless employee_role.present?
+
+    @assigned_benefit_package ||= employee_role.benefit_package(qle: context.qle, shop_under_current: context.shop_under_current, shop_under_future: context.shop_under_future)
+  end
+
+  def possible_benefit_package
+    @possible_benefit_package ||= previous_hbx_enrollment.sponsored_benefit_package if context.change_plan.present? && previous_hbx_enrollment.present?
+  end
+
+  def previous_hbx_enrollment
+    context.previous_hbx_enrollment
+  end
+
+  def employee_role
+    context.employee_role
   end
 end
