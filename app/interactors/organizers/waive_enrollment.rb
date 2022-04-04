8597e161ee7d1e9b4249bf92fc51fd253e60496a
Vishal Kalletla <vishuk1201@gmail.com>
2022-04-04 16:39:23 -0400

    wire existing waiver functionality to continuous flow

---
+++ app/interactors/organizers/waive_enrollment.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Organizers
+  class WaiveEnrollment
+    include Interactor::Organizer
+
+    organize [
+      FindEnrollment,
+      ConstructWaiverEnrollment,
+      TriggerWaive]
+  end
+end
