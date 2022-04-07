c623df5597034109fd2b9a9003ab4b827aba48bc
annscaballero <67475026+annscaballero@users.noreply.github.com>
2022-04-07 15:02:27 -0400

    181281065 continuous shopping pom implementation (#2199)
    * continuous plan shopping POM
    
    * continuous shopping POM
    
    * missing data cukes
    
    * missing data cukes

---
+++ features/support/object_model_pages/shop/employee/ee_choose_coverage.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+#insured/members_selections/new?change_plan
+class EmployeeChooseCoverage
+
+  def self.enroll_health
+    '[data-cuke="health-enroll-radio"]'
+  end
+
+  def self.waive_health
+    '[data-cuke="health-waive-radio"]'
+  end
+
+  def self.enroll_dental
+    '[data-cuke="dental-enroll-radio"]'
+  end
+
+  def self.waive_dental
+    '[data-cuke="dental-waive-radio"]'
+  end
+
+  def self.shop_for_new_plan_btn
+    'input[class$="interaction-click-control-shop-for-new-plan"]'
+  end
+
+  def self.back_to_my_account_btn
+    'a[class$="interaction-click-control-back-to-my-account"]'
+  end
+end
