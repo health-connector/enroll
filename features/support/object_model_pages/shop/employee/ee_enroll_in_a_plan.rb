2d2205315426b72312c12e3f482c127ff5446dc1
annscaballero <67475026+annscaballero@users.noreply.github.com>
2022-04-07 15:02:27 -0400

    181281065 continuous shopping pom implementation (#2199)
    * continuous plan shopping POM
    
    * continuous shopping POM
    
    * missing data cukes
    
    * missing data cukes

---
+++ features/support/object_model_pages/shop/employee/ee_enroll_in_a_plan.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+#insured/product_shoppings/continuous_show
+class EmployeeEnrollInAPlan
+
+  def self.find_your_doctor_btn
+    'button[class$="interaction-click-control-find-your-doctor"]'
+  end
+
+  def self.plan_name_btn
+    'a[class$="interaction-click-control-plan-name"]'
+  end
+
+  def self.premium_amount_btn
+    'a[class$="interaction-click-control-premium-amount"]'
+  end
+
+  def self.deductible_btn
+    'a[class$="interaction-click-control-deductible"]'
+  end
+
+  def self.carrier_btn
+    'a[class$="interaction-click-control-carrier"]'
+  end
+
+  def self.select_plan_btn
+    'a[class$="interaction-click-control-select-plan"]'
+  end
+
+  def self.see_details_btn
+    'a[class$="interaction-click-control-see-details"]'
+  end
+
+  def self.back_to_results_btn
+    'a[class$="all-plans"]'
+  end
+end
