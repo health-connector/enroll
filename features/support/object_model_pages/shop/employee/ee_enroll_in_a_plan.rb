ac7e893f6104f6d2261a7f9647cb2c45d7ea4c42
Ryan Eddy <44847768+RyanEddyIC@users.noreply.github.com>
2022-04-14 12:22:35 -0400

    Enable product filters in continuous shopping flow (#2202)
    * enable plan filters in continuos shopping flow
    
    * add frozen string comment
    
    * remove nationwide markets feature step

--- features/support/object_model_pages/shop/employee/ee_enroll_in_a_plan.rb
+++ features/support/object_model_pages/shop/employee/ee_enroll_in_a_plan.rb
@@ -42,4 +42,64 @@ class EmployeeEnrollInAPlan
   def self.health_header_text
     'Enroll in a Health Plan'
   end
+
+  def self.filtered_plan
+    '[data-cuke="filtered-plan"]'
+  end
+
+  def self.plan_type_filter
+    '[data-cuke="plan-type-filter"]'
+  end
+
+  def self.plan_metal_level_filter
+    '[data-cuke="plan-metal-level-filter"]'
+  end
+
+  def self.plan_network_filter
+    '[data-cuke="plan-network-filter"]'
+  end
+
+  def self.plan_network_filter_option
+    '[data-cuke="plan-network-filter"]'
+  end
+
+  def self.plan_carrier_filter
+    '[data-cuke="plan-carrier-filter"]'
+  end
+
+  def self.plan_carrier_filter_option
+    '[data-cuke="plan-carrier-filter"] li'
+  end
+
+  def self.plan_hsa_filter
+    '[data-cuke="plan-hsa-filter"]'
+  end
+
+  def self.plan_hsa_filter_option
+    '[data-cuke="plan-hsa-filter"] li'
+  end
+
+  def self.plan_premium_filter
+    '[data-cuke="plan-premium-filter"]'
+  end
+
+  def self.plan_premium_filter_from
+    'plan-premium-from'
+  end
+
+  def self.plan_deductible_filter
+    '[data-cuke="plan-deductible-filter"]'
+  end
+
+  def self.plan_deductible_filter_from
+    'plan-deductible-from'
+  end
+
+  def self.apply_filters_btn
+    '[data-cuke="apply-plan-filters-btn"]'
+  end
+
+  def self.reset_filters_btn
+    '[data-cuke="reset-plan-filters-btn"]'
+  end
 end
