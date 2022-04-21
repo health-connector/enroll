1797aaf204ec32bda95f5066102d28dc4495fbb2
Jay Reddy <jayanth.rordev@gmail.com>
2022-04-21 08:15:06 -0400

    cucumbers dental confirmation page (#2217)
    * cucumbers dental confirmation page
    
    * updates to choose your household verbiage

--- features/support/object_model_pages/shop/employee/ee_enroll_in_a_plan.rb
+++ features/support/object_model_pages/shop/employee/ee_enroll_in_a_plan.rb
@@ -102,4 +102,28 @@ class EmployeeEnrollInAPlan
   def self.reset_filters_btn
     '[data-cuke="reset-plan-filters-btn"]'
   end
+
+  def self.dual_enrollment_text
+    '[data-cuke="dual-enrollment-text"]'
+  end
+
+  def self.ee_choose_coverage
+    '[class$="interaction-choice-control-value-eligible-continue-no"]'
+  end
+
+  def self.continue_coverage_button
+    '.interaction-click-control-continue'
+  end
+
+  def self.dental_enrollment_confirmation
+    'Your dental'
+  end
+
+  def self.available_coverage
+    '[data-cuke="available-coverage"]'
+  end
+
+  def self.shop_for_text
+    '[data-cuke="shop-for"]'
+  end
 end
