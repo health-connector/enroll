d1865fefac41316ce9bbfdf4c143ec65712b84b8
ymhari <hariym471@gmail.com>
2022-04-29 11:12:19 -0400

    Adds spec coverage.

--- spec/interactors/fetch_shop_benefit_spec.rb
+++ spec/interactors/fetch_shop_benefit_spec.rb
@@ -1,29 +1,12 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require "#{BenefitSponsors::Engine.root}/spec/shared_contexts/benefit_market.rb"
-require "#{BenefitSponsors::Engine.root}/spec/shared_contexts/benefit_application.rb"
+require "./spec//shared_context/setup_shop_families_enrollments"
 
 describe FetchShopBenefit, :dbclean => :after_each do
+  include_context "setup shop families enrollments"
+
   context "when shopping in OE" do
-    include_context "setup benefit market with market catalogs and product packages"
-    include_context "setup initial benefit application"
-    include_context "setup employees with benefits"
-
-    let!(:ce) { benefit_sponsorship.census_employees.first }
-    let!(:ee_person) { FactoryGirl.create(:person, :with_employee_role, :with_family, first_name: ce.first_name, last_name: ce.last_name, dob: ce.dob, ssn: ce.ssn, gender: ce.gender) }
-    let!(:employee_role) do
-      ee_person.employee_roles.first.update_attributes!(employer_profile: abc_profile)
-      ee_person.employee_roles.first
-    end
-
-    before :each do
-      ce.employee_role_id = ee_person.employee_roles.first.id
-      ce.save
-      ee_person.employee_roles.first.census_employee_id = ce.id
-      ee_person.save
-    end
-
     it "should fetch employee_role" do
       context = described_class.call(employee_role: employee_role, market_kind: "shop")
       expect(context.benefit_package).to be_truthy
