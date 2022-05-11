1b5261ed67868dcfc8c26da0c699194e075c4e39
RyanEddyIC <ryan.eddy@ideacrew.com>
2022-05-11 15:13:31 -0400

    rearrange dependent form steps to fix cucumber

--- features/step_definitions/integration_steps.rb
+++ features/step_definitions/integration_steps.rb
@@ -792,10 +792,10 @@ When(/^.+ enters? the dependent info of .+ daughter$/) do
   fill_in 'dependent[last_name]', with: 'White'
   date = TimeKeeper.date_of_record - 28.years
   dob = date.to_s
-  fill_in 'jq_datepicker_ignore_dependent[dob]', with: dob
   find(:xpath, "//p[@class='label'][contains(., 'This Person Is')]").click
   find(:xpath, "//li[@data-index='3'][contains(., 'Child')]").click
   find(:xpath, "//label[@for='radio_female']").click
+  fill_in 'jq_datepicker_ignore_dependent[dob]', with: dob
 end
 
 When(/^.+ enters? the dependent info of Patrick wife$/) do
