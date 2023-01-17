60603dfbf7d79469cc6fb7eb0ecab181ef349db0
rahul <rahul.gudimetla@ideacrew.com>
2023-01-17 08:08:28 -0600

    fix typo

--- components/benefit_sponsors/app/controllers/benefit_sponsors/benefit_packages/benefit_packages_controller.rb
+++ components/benefit_sponsors/app/controllers/benefit_sponsors/benefit_packages/benefit_packages_controller.rb
@@ -82,7 +82,11 @@ module BenefitSponsors
         @benefit_application = @benefit_sponsorship.benefit_applications.where(id: application_id).first
         @benefit_package = @benefit_application.benefit_packages.where(id: params[:id]).first
 
-        @employee_costs_result = ::BenefitSponsors::Operations::BenefitSponsorship::EstimatedEmployeeCosts.new.call({}).value!
+        @employee_costs_result = ::BenefitSponsors::Operations::BenefitSponsorship::EstimatedEmployeeCosts.new.call({
+                                                                                                                      benefit_application: @benefit_application,
+                                                                                                                      benefit_package: @benefit_package,
+                                                                                                                      package_kind: params[:kind]
+                                                                                                                    }).value!
         @employee_costs = Kaminari.paginate_array(@employee_costs_result[:employee_costs]).page(params[:page]).per(5)
       end
 
