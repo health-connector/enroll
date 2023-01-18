8c4e190146fbc274481d72c730915cb28dd752e1
Nisanth Yaganti <yagantinisanth@gmail.com>
2023-01-18 10:30:28 -0500

    style updates

--- components/benefit_sponsors/app/domain/benefit_sponsors/operations/benefit_sponsorship/estimated_employee_costs.rb
+++ components/benefit_sponsors/app/domain/benefit_sponsors/operations/benefit_sponsorship/estimated_employee_costs.rb
@@ -28,15 +28,16 @@ module BenefitSponsors
 
         def estimated_employee_costs(values)
           sponsored_benefit = if values[:package_kind] == "dental"
-                                values[:benefit_package].sponsored_benefits.where(_type: "BenefitSponsors::SponsoredBenefits::DentalSponsoredBenefit").first
+                                values[:benefit_package].dental_sponsored_benefit
                               else
-                                values[:benefit_package].sponsored_benefits.where(_type: "BenefitSponsors::SponsoredBenefits::HealthSponsoredBenefit").first
+                                values[:benefit_package].health_sponsored_benefit
                               end
           product_package = sponsored_benefit.product_package
 
           estimator = ::BenefitSponsors::Services::SponsoredBenefitCostEstimationService.new
           @employee_costs = estimator.calculate_employee_estimates_for_all_products_in_package(values[:benefit_application], sponsored_benefit, sponsored_benefit.reference_product, product_package)
           @employer_estimated_costs = estimator.calculate_estimates_for_package_edit(values[:benefit_application], sponsored_benefit, sponsored_benefit.reference_product, product_package)
+
           result = { employee_costs: @employee_costs, employer_estimated_costs: @employer_estimated_costs, reference_product: sponsored_benefit.reference_product }
           Success(result)
         end
