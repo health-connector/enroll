a273830315ddac9f99386483cdb5529eb8dd37fd
RyanEddyIC <ryan.eddy@ideacrew.com>
2023-07-13 15:41:29 -0400

    add sole proprietor to entity kinds list

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -14,6 +14,7 @@ class Organization
     "tax_exempt_organization",
     "c_corporation",
     "s_corporation",
+    "sole_proprietor",
     "partnership",
     "limited_liability_corporation",
     "limited_liability_partnership",
