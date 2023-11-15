cf641acebf23a2b12c5158436a6a1705620f6f13
Nisanth Yaganti <yagantinisanth@gmail.com>
2023-11-15 06:33:52 -0500

    setting up benefit application item & composite

---
+++ components/benefit_sponsors/app/models/benefit_sponsors/benefit_applications/benefit_application_item.rb
@@ -0,0 +1,34 @@
+module BenefitSponsors
+  class BenefitApplications::BenefitApplicationItem
+    include Mongoid::Document
+    include Mongoid::Timestamps
+
+    embedded_in :benefit_application,
+                class_name: "::BenefitSponsors::BenefitApplications::BenefitApplication",
+                inverse_of: :benefit_application_items
+
+    VOLUNTARY_TERM_REASONS = [
+      "Company went out of business/bankrupt",
+      "Customer Service did not solve problem/poor experience",
+      "Connector website too difficult to use/navigate",
+      "Health Connector does not offer desired product",
+      "Group is now > 50 lives",
+      "Group no longer has employees",
+      "Went to carrier directly",
+      "Went to an association directly",
+      "Added/changed broker that does not work with Health Connector",
+      "Company is no longer offering insurance",
+      "Company moved out of Massachusetts",
+      "Other"
+    ].freeze
+
+    NON_PAYMENT_TERM_REASONS = [
+      "Non-payment of premium"
+    ].freeze
+
+    field :effective_period,        type: Range
+    field :sequence_id,        type: Integer
+    field :event_kind,       type: String # TODO: - come up with better name
+    field :event_reason,     type: String # TODO: - come up with better name
+  end
+end
