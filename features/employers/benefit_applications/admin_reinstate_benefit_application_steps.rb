73e78440a3e6b634f752b5d7505a6e5164ab052a
nks2109 <nikhilks219@gmail.com>
2023-11-16 00:53:04 -0500

    adding feature test cases

--- features/employers/benefit_applications/admin_reinstate_benefit_application_steps.rb
+++ features/employers/benefit_applications/admin_reinstate_benefit_application_steps.rb
@@ -7,3 +7,15 @@ end
 Given(/^the Reinstate feature configuration is disabled$/) do
   disable_feature :benefit_application_reinstate
 end
+
+And(/^initial employer ABC Widgets has updated (.*) effective period for reinstate$/) do |aasm_state|
+  if aasm_state == 'terminated'
+    employer_profile.benefit_applications.first.workflow_state_transitions << WorkflowStateTransition.new(from_state: 'active', to_state: 'terminated', event: 'terminate!')
+    term_ba = employer_profile.benefit_applications.first
+    start_on = term_ba.benefit_sponsor_catalog.effective_period.min
+    end_on = term_ba.benefit_sponsor_catalog.effective_period.max
+    effective_period = start_on..end_on
+    term_ba.benefit_sponsor_catalog.update_attributes!(effective_period: effective_period)
+    employer_profile.benefit_applications.first.update_attributes!(effective_period: effective_period)
+  end
+end
