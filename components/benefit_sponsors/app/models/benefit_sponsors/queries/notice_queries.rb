8329ee23e1c2527f2259b8c39bb262401fb3cb85
HArsHA Ellanki <h_vardhan@yahoo.com>
2023-12-08 16:09:48 -0500

    rubocop fixes

--- components/benefit_sponsors/app/models/benefit_sponsors/queries/notice_queries.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/queries/notice_queries.rb
@@ -1,52 +1,50 @@
+# frozen_string_literal: true
+
 module BenefitSponsors
   module Queries
     class NoticeQueries
 
-      def self.initial_employers_by_effective_on_and_state(start_on: TimeKeeper.date_of_record, aasm_state:)
+      def self.initial_employers_by_effective_on_and_state(aasm_state:, start_on: TimeKeeper.date_of_record)
         BenefitSponsors::BenefitSponsorships::BenefitSponsorship.where({
-          :'benefit_applications' => {:"$elemMatch" => {
-              :aasm_state => aasm_state,
-              :"benefit_application_items.effective_period.min" => start_on,
-            }
-          }
-        })
+                                                                         :benefit_applications => {:"$elemMatch" => {
+                                                                           :aasm_state => aasm_state,
+                                                                           :"benefit_application_items.effective_period.min" => start_on
+                                                                         }}
+                                                                       })
       end
 
       def self.organizations_for_force_publish(new_date)
         BenefitSponsors::BenefitSponsorships::BenefitSponsorship.where({
-          :'benefit_applications' => {:"$elemMatch" => {
-            :aasm_state => :draft,
-            :"benefit_application_items.effective_period.min" => new_date.next_month.beginning_of_month,
-          }}
-        })
+                                                                         :benefit_applications => {:"$elemMatch" => {
+                                                                           :aasm_state => :draft,
+                                                                           :"benefit_application_items.effective_period.min" => new_date.next_month.beginning_of_month
+                                                                         }}
+                                                                       })
       end
 
       def self.organizations_for_low_enrollment_notice(current_date)
         BenefitSponsors::BenefitSponsorships::BenefitSponsorship.where({
-          :'benefit_applications' => {:"$elemMatch" => {
-            :"aasm_state" => :enrollment_open,
-            :"open_enrollment_period.max" => current_date + 2.days
-            }
-          }
-        })
+                                                                         :benefit_applications => {:"$elemMatch" => {
+                                                                           :aasm_state => :enrollment_open,
+                                                                           :"open_enrollment_period.max" => current_date + 2.days
+                                                                         }}
+                                                                       })
       end
 
       def self.initial_employers_in_enrolled_state
         BenefitSponsors::BenefitSponsorships::BenefitSponsorship.where({
-          :"benefit_applications" => {:"$elemMatch" => {
-              :"aasm_state" => :enrollment_closed,
-            }
-          }
-        })
+                                                                         :benefit_applications => {:"$elemMatch" => {
+                                                                           :aasm_state => :enrollment_closed
+                                                                         }}
+                                                                       })
       end
 
       def self.initial_employers_in_ineligible_state
         BenefitSponsors::BenefitSponsorships::BenefitSponsorship.where({
-          :"benefit_applications" => {:"$elemMatch" => { 
-              :"aasm_state" => :enrollment_ineligible,
-            }
-          }
-        })
+                                                                         :benefit_applications => {:"$elemMatch" => {
+                                                                           :aasm_state => :enrollment_ineligible
+                                                                         }}
+                                                                       })
       end
     end
   end
