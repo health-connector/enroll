07827e49d110eaa92399a73fcb5a60c4b02f5197
nks2109 <nikhilks219@gmail.com>
2023-12-15 16:07:19 -0500

    handle sep query

--- script/shop_sep_query.rb
+++ script/shop_sep_query.rb
@@ -89,16 +89,20 @@ def term_states
 end
 
 def can_publish_enrollment?(enrollment, transition_at)
-  sb = enrollment.sponsored_benefit
-  benefit_application = sb.benefit_package.benefit_application
-  quiet_period = benefit_application.enrollment_quiet_period
-  if is_valid_benefit_application?(benefit_application)
-    return false if transition_at.in_time_zone("UTC") <= quiet_period.max # don't transmit enrollments until quiet period ended
-    return true  if term_states.include?(enrollment.aasm_state) # new hire enrollment check not needed for terminated enrollments
-    return false if enrollment.new_hire_enrollment_for_shop? && (enrollment.effective_on <= (Time.now - 2.months))
-    return true
-  else
-    return false
+  begin
+    sb = enrollment.sponsored_benefit
+    benefit_application = sb.benefit_package.benefit_application
+    quiet_period = benefit_application.enrollment_quiet_period
+    if is_valid_benefit_application?(benefit_application)
+      return false if transition_at.in_time_zone("UTC") <= quiet_period.max # don't transmit enrollments until quiet period ended
+      return true  if term_states.include?(enrollment.aasm_state) # new hire enrollment check not needed for terminated enrollments
+      return false if enrollment.new_hire_enrollment_for_shop? && (enrollment.effective_on <= (Time.now - 2.months))
+      return true
+    else
+      return false
+    end
+  rescue => e
+    puts "subscriber_hbx_id: #{enrollment&.subscriber&.hbx_id}, #{e}, stacktrace: #{e.backtrace}"
   end
 end
 
@@ -107,44 +111,62 @@ puts term_ids.length unless Rails.env.test?
 
 purchase_families = Family.where("households.hbx_enrollments.hbx_id" => {"$in" => purchase_ids})
 
+puts "-----purchased families #{purchase_ids}"
 Rails.logger.info "-----purchased families #{purchase_ids}"
 purchase_families.each do |fam|
-  purchases = fam.households.flat_map(&:hbx_enrollments).select { |en| purchase_ids.include?(en.hbx_id) }
-  purchases.each do |purchase|
-    purchased_at = purchase.workflow_state_transitions.where({
-      "to_state" => {"$in" => ["coverage_selected", "auto_renewing"]},
-      "transition_at" => {
-        "$gte" => start_time,
-        "$lt" => end_time
-      }
-    }).first.transition_at
+  begin
+    purchases = fam.households.flat_map(&:hbx_enrollments).select { |en| purchase_ids.include?(en.hbx_id) }
+    purchases.each do |purchase|
+      begin
+        purchased_at = purchase.workflow_state_transitions.where({
+          "to_state" => {"$in" => ["coverage_selected", "auto_renewing"]},
+          "transition_at" => {
+            "$gte" => start_time,
+            "$lt" => end_time
+          }
+        }).first.transition_at
 
-    Rails.logger.info "---processing #{purchase.hbx_id}---#{purchased_at}---#{Time.now}"
-    if can_publish_enrollment?(purchase, purchased_at)
-      Rails.logger.info "-----publishing #{purchase.hbx_id}"
-      ShopEnrollmentsPublisher.publish_action( "acapi.info.events.hbx_enrollment.coverage_selected",
-                     purchase.hbx_id,
-                     "urn:openhbx:terms:v1:enrollment#initial")
+        puts "---processing #{purchase.hbx_id}---#{purchased_at}---#{Time.now}, "
+        Rails.logger.info "---processing #{purchase.hbx_id}---#{purchased_at}---#{Time.now}"
+        if can_publish_enrollment?(purchase, purchased_at)
+          Rails.logger.info "-----publishing #{purchase.hbx_id}"
+          ShopEnrollmentsPublisher.publish_action( "acapi.info.events.hbx_enrollment.coverage_selected",
+                         purchase.hbx_id,
+                         "urn:openhbx:terms:v1:enrollment#initial")
+        end
+      rescue => e
+        puts "enrollment_hbx_id: #{purchase&.hbx_id}, #{e}, stacktrace: #{e.backtrace}"
+      end
     end
+  rescue => e
+    puts "initial transaction query, issue Family_id: #{fam.id}, #{e}, stacktrace: #{e.backtrace}"
   end
 end
 
 term_families = Family.where("households.hbx_enrollments.hbx_id" => {"$in" => term_ids})
 term_families.each do |fam|
-  terms = fam.households.flat_map(&:hbx_enrollments).select { |en| term_ids.include?(en.hbx_id) }
-  terms.each do |term|
-    terminated_at = term.workflow_state_transitions.where({
-      "to_state" => {"$in" => term_states},
-      "transition_at" => {
-        "$gte" => start_time,
-        "$lt" => end_time
-      }
-    }).first.transition_at
+  begin
+    terms = fam.households.flat_map(&:hbx_enrollments).select { |en| term_ids.include?(en.hbx_id) }
+    terms.each do |term|
+      begin
+        terminated_at = term.workflow_state_transitions.where({
+          "to_state" => {"$in" => term_states},
+          "transition_at" => {
+            "$gte" => start_time,
+            "$lt" => end_time
+          }
+        }).first.transition_at
 
-    if can_publish_enrollment?(term, terminated_at)
-      ShopEnrollmentsPublisher.publish_action( "acapi.info.events.hbx_enrollment.terminated",
-                     term.hbx_id,
-                     "urn:openhbx:terms:v1:enrollment#terminate_enrollment")
+        if can_publish_enrollment?(term, terminated_at)
+          ShopEnrollmentsPublisher.publish_action( "acapi.info.events.hbx_enrollment.terminated",
+                         term.hbx_id,
+                         "urn:openhbx:terms:v1:enrollment#terminate_enrollment")
+        end
+      rescue => e
+        puts "enrollment_hbx_id: #{term&.hbx_id}, #{e}, stacktrace: #{e.backtrace}"
+      end
     end
+  rescue => e
+    puts "term transaction query, issue Family_id: #{fam.id}, #{e}, stacktrace: #{e.backtrace}"
   end
 end
