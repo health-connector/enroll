8c16145a0bce66a3fd863ceac697def4397a60cc
Hanna <hanna.kalesnikava@ideacrew.com>
2024-03-20 10:22:11 -0700

    Person callbacks revert

--- app/models/person.rb
+++ app/models/person.rb
@@ -248,8 +248,8 @@ class Person
 
   validate :consumer_fields_validations
 
-  # after_create :notify_created
-  # after_update :notify_updated
+  after_create :notify_created
+  after_update :notify_updated
 
   def active_general_agency_staff_roles
     general_agency_staff_roles.select(&:active?)
