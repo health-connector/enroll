57a2dc05731556af9a8b829902802612210602d7
rahul <rahul.gudimetla@ideacrew.com>
2024-04-01 10:35:17 -0500

    fix spec views

--- spec/views/insured/families/_navigation.html.erb_spec.rb
+++ spec/views/insured/families/_navigation.html.erb_spec.rb
@@ -1,33 +1,14 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe "insured/families/_navigation.html.erb" do
   let(:person) {FactoryBot.create(:person, :with_family)}
   let(:user){ FactoryBot.create(:user, person: person) }
-
-  # let(:person){
-  #   instance_double(
-  #     "Person",
-  #     first_name: "my first name",
-  #     last_name: "my last name"
-  #     )
-  # }
   let(:employee_role){ instance_double("EmployeeRole") }
   let(:employer_profile){ instance_double("EmployerProfile") }
   let(:broker_agency_profile){ instance_double("BrokerAgencyProfile") }
   let(:inbox){ instance_double("Inbox") }
-  let(:active_family_members){ instance_double("ActiveFamilyMembers")}
-  def family_member
-    random_value = rand(999_999_999)
-    instance_double(
-      "FamilyMember#{random_value}",
-      first_name: "my real first name #{random_value}",
-      last_name: "my real last name #{random_value}"
-    )
-  end
-
-  let(:family_members){ [family_member, family_member] }
+  let(:family_member) { FamilyMember.new(:is_primary_applicant => nil, :is_coverage_applicant => nil, :person => person) }
+  let(:family_members){ [ family_member, family_member ] }
 
   before :each do
     sign_in(user)
@@ -35,6 +16,7 @@ RSpec.describe "insured/families/_navigation.html.erb" do
     allow(person).to receive(:has_active_employee_role?).and_return(true)
     allow(person).to receive(:has_consumer_role?).and_return(true)
     allow(person).to receive(:has_resident_role?).and_return(true)
+    allow(person).to receive(:has_active_consumer_role?).and_return(true)
     allow(person).to receive(:inbox).and_return(inbox)
     allow(view).to receive(:enrollment_group_unverified?)
     allow(view).to receive(:verification_needed?)
@@ -65,7 +47,8 @@ RSpec.describe "insured/families/_navigation.html.erb" do
 
       it "should display the info under my account in the side bar" do
         expect(rendered).to have_text('My Account')
-        expect(rendered).to have_text("#{person.first_name} #{person.last_name}")
+        expect(rendered).to have_text("#{person.first_name}")
+        expect(rendered).to have_text("#{person.last_name}")
         expect(rendered).to have_text("Your Household of #{family_members.count}")
       end
 
@@ -85,7 +68,7 @@ RSpec.describe "insured/families/_navigation.html.erb" do
     end
     describe "no unverified enrollments" do
       before :each do
-        allow(view).to receive(:enrollment_group_unverified?).and_return true
+        allow(view).to receive(:enrollment_group_unverified?).and_return false
         allow(view).to receive(:verification_needed?).and_return false
         render partial: "insured/families/navigation.html.erb"
       end
@@ -98,8 +81,9 @@ RSpec.describe "insured/families/_navigation.html.erb" do
       end
 
       it "should display the info under my account in the side bar" do
-        expect(rendered).to have_text('My Account')
-        expect(rendered).to have_text("#{person.first_name} #{person.last_name}")
+        expect(rendered).to have_text( 'My Account')
+        expect(rendered).to have_text("#{person.first_name}" )
+        expect(rendered).to have_text("#{person.last_name}")
         expect(rendered).to have_text("Your Household of #{family_members.count}")
       end
 
@@ -120,7 +104,7 @@ RSpec.describe "insured/families/_navigation.html.erb" do
 
     describe "with uploaded documents" do
       before :each do
-        allow(view).to receive(:enrollment_group_unverified?).and_return true
+        allow(view).to receive(:enrollment_group_unverified?).and_return false
         allow(view).to receive(:verification_needed?).and_return true
         allow(view).to receive(:documents_uploaded).and_return true
         render partial: "insured/families/navigation.html.erb"
@@ -134,8 +118,9 @@ RSpec.describe "insured/families/_navigation.html.erb" do
       end
 
       it "should display the info under my account in the side bar" do
-        expect(rendered).to have_text('My Account')
-        expect(rendered).to have_text("#{person.first_name} #{person.last_name}")
+        expect(rendered).to have_text( 'My Account')
+        expect(rendered).to have_text("#{person.first_name}" )
+        expect(rendered).to have_text("#{person.last_name}")
         expect(rendered).to have_text("Your Household of #{family_members.count}")
       end
 
@@ -171,7 +156,7 @@ RSpec.describe "insured/families/_navigation.html.erb" do
 
     it "should display the info under my account in the side bar" do
       expect(rendered).to have_text('My Account')
-      expect(rendered).to have_text("#{person.first_name} #{person.last_name}")
+      expect(rendered).to have_text("#{person.first_name}")
       expect(rendered).to have_text("Your Household of #{family_members.count}")
     end
 
