2035851715469ea2bc8a445a20f289d331f09632
Marco (Polo) Ornelas <marco.ornelas@ideacrew.com>
2024-04-02 15:04:49 -0400

    disable rspec; update webdriver parameter

--- features/support/env.rb
+++ features/support/env.rb
@@ -78,9 +78,10 @@ end
 
 Capybara.register_driver :selenium_chrome_custom do |app|
   options = Selenium::WebDriver::Chrome::Options.new
-  options.headless!
+  # options.headless!
   options.add_argument("no-sandbox")
   options.add_argument("--window-size=1024,768")
+  options.add_argument("--headless=new")
 
   if RUBY_PLATFORM =~ /darwin/
     options.add_argument("--enable-features=NetworkService,NetworkServiceInProcess")
