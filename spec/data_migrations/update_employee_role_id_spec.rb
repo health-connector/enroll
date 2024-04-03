5f9427efa348b978c977f536aca8a9c25ed6e00f
rahul <rahul.gudimetla@ideacrew.com>
2024-04-02 20:39:10 -0500

    fix blocks

--- spec/data_migrations/update_employee_role_id_spec.rb
+++ spec/data_migrations/update_employee_role_id_spec.rb
@@ -53,7 +53,7 @@ describe UpdateEmployeeRoleId, dbclean: :after_each do
       application = FactoryBot.create(:benefit_sponsors_benefit_application, :with_benefit_sponsor_catalog, benefit_sponsorship: benefit_sponsorship, default_effective_period: effective_period, aasm_state: :active)
       application.benefit_sponsor_catalog.save!
       application
-    end
+    }
 
     let!(:benefit_package) { FactoryBot.create(:benefit_sponsors_benefit_packages_benefit_package, benefit_application: benefit_application, product_package: product_package) }
     let(:benefit_group_assignment) {FactoryBot.build(:benefit_sponsors_benefit_group_assignment, benefit_group: benefit_package)}
