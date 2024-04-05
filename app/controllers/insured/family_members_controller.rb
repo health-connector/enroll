d3c19a255ebd3a5936467f634ad055556320fc92
utkarsh7989 <utkarsh7989@gmail.com>
2024-04-05 04:15:29 -0400

    rubocop fix

--- app/controllers/insured/family_members_controller.rb
+++ app/controllers/insured/family_members_controller.rb
@@ -24,7 +24,6 @@ class Insured::FamilyMembersController < ApplicationController
         Rails.logger.error(exception_message) unless Rails.env.test?
         redirect_to root_path and return
       end
-      binding.irb
       redirect_to resident_index_insured_family_members_path(:resident_role_id => @person.resident_role.id, :change_plan => params[:change_plan], :qle_date => params[:qle_date], :qle_id => params[:qle_id], :effective_on_kind => params[:effective_on_kind], :qle_reason_choice => params[:qle_reason_choice], :commit => params[:commit])
     end
 
