8090270c727049aec948c2c3de1dd7785bd91754
rahul <rahul.gudimetla@ideacrew.com>
2024-04-12 13:29:56 -0500

    fix mongo connections in mongo

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,6 +11,19 @@ worker_processes 16
 timeout 30
 preload_app true
 
+# Disconnect MongoDB clients before forking new worker processes
+before_fork do |server, worker|
+  Mongoid.disconnect_clients
+end
+
+# Close and reconnect MongoDB clients on worker boot
+on_worker_boot do
+  Mongoid::Clients.clients.each do |_name, client|
+    client.close
+    client.reconnect
+  end
+end
+
 after_fork do |server, worker|
   Acapi::Requestor.reconnect!
   Acapi::LocalAmqpPublisher.reconnect!
