ea0ed898a0bd0eba62ef1825da557544e203fcd4
nks2109 <nikhilks219@gmail.com>
2024-04-16 10:33:41 -0400

    rubocop fix

--- app/controllers/individual_market/documents_controller.rb
+++ app/controllers/individual_market/documents_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module IndividualMarket
   class DocumentsController < ApplicationController
     before_action :set_person, only: [:fed_hub_request, :enrollment_verification, :update_verification_type, :extend_due_date]
@@ -9,9 +11,9 @@ module IndividualMarket
       update_reason = params[:verification_reason]
       admin_action = params[:admin_action]
       reasons_list = VlpDocument::VERIFICATION_REASONS + VlpDocument::ALL_TYPES_REJECT_REASONS + VlpDocument::CITIZEN_IMMIGR_TYPE_ADD_REASONS
-      if (reasons_list).include? (update_reason)
+      if (reasons_list).include?(update_reason)
         verification_result = @person.consumer_role.admin_verification_action(admin_action, v_type, update_reason)
-        message = (verification_result.is_a? String) ? verification_result : "Person verification successfully approved."
+        message = verification_result.is_a?(String) ? verification_result : "Person verification successfully approved."
         flash_message = { :success => message}
         update_documents_status(family_member) if family_member
       else
@@ -51,11 +53,11 @@ module IndividualMarket
         @person.consumer_role.redetermine_verification!(verification_attr)
       end
       respond_to do |format|
-        format.html {
-          hub =  params[:verification_type] == 'DC Residency' ? 'Local Residency' : 'FedHub'
+        format.html do
+          hub = params[:verification_type] == 'DC Residency' ? 'Local Residency' : 'FedHub'
           flash[:success] = "Request was sent to #{hub}."
           redirect_to :back
-        }
+        end
         format.js
       end
     end
@@ -63,27 +65,14 @@ module IndividualMarket
     def extend_due_date
       @family_member = FamilyMember.find(params[:family_member_id])
       v_type = params[:verification_type]
-      enrollment = @family_member.family.enrollments.verification_needed.where(:"hbx_enrollment_members.applicant_id" => @family_member.id).first
-      if enrollment.present?
-        add_type_history_element
-        sv = @family_member.person.consumer_role.special_verifications.where(:"verification_type" => v_type).order_by(:"created_at".desc).first
-        if sv.present?
-          new_date = sv.due_date.to_date + 30.days
-          flash[:success] = "Special verification period was extended for 30 days."
-        else
-          new_date = (enrollment.submitted_at.to_date + 95.days) + 30.days
-          flash[:success] = "You set special verification period for this Enrollment. Verification due date now is #{new_date.to_date}"
-        end
+      enrollment = find_enrollment
 
-        # special_verification_period is the day we send notices
-        # enrollment.update_attributes!(:special_verification_period => new_date)
-        sv = SpecialVerification.new(due_date: new_date, verification_type: v_type, updated_by: current_user.id, type: "admin")
-        @family_member.person.consumer_role.special_verifications << sv
-        @family_member.person.consumer_role.save!
-        set_min_due_date_on_family
+      if enrollment.present?
+        process_enrollment(enrollment, v_type)
       else
         flash[:danger] = "Family Member does not have any unverified Enrollment to extend verification due date."
       end
+
       redirect_to :back
     end
 
@@ -99,12 +88,10 @@ module IndividualMarket
       action = params[:admin_action] || params[:action]
       action = "Delete #{params[:doc_title]}" if action == "destroy"
       reason = params[:verification_reason]
-      if @person
-        @person.consumer_role.add_type_history_element(verification_type: verification_type,
+      @person&.consumer_role&.add_type_history_element(verification_type: verification_type,
                                                        action: action.split('_').join(' '),
                                                        modifier: actor,
                                                        update_reason: reason)
-      end
     end
 
     def update_documents_status(family_member)
@@ -114,8 +101,35 @@ module IndividualMarket
 
     def verification_attr
       OpenStruct.new({:determined_at => Time.now,
-                      :authority => "hbx"
-                     })
+                      :authority => "hbx"})
+    end
+
+    def find_enrollment
+      @family_member.family.enrollments.verification_needed.where(:"hbx_enrollment_members.applicant_id" => @family_member.id).first
+    end
+
+    def process_enrollment(enrollment, v_type)
+      add_type_history_element
+      special_verification = @family_member.person.consumer_role.special_verifications.where(:verification_type => v_type).order_by(:created_at.desc).first
+      new_date = calculate_new_date(special_verification, enrollment)
+      create_special_verification(new_date, v_type)
+      set_min_due_date_on_family
+    end
+
+    def calculate_new_date(special_verification, enrollment)
+      if special_verification.present?
+        flash[:success] = "Special verification period was extended for 30 days."
+        special_verification.due_date.to_date + 30.days
+      else
+        flash[:success] = "You set special verification period for this Enrollment. Verification due date now is #{new_date.to_date}"
+        (enrollment.submitted_at.to_date + 95.days) + 30.days
+      end
+    end
+
+    def create_special_verification(new_date, v_type)
+      sv = SpecialVerification.new(due_date: new_date, verification_type: v_type, updated_by: current_user.id, type: "admin")
+      @family_member.person.consumer_role.special_verifications << sv
+      @family_member.person.consumer_role.save!
     end
 
     def set_min_due_date_on_family
