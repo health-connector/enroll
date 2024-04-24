e32e52abc4efa0b5d06f68d81fb47e72db297c25
rahul <rahul.gudimetla@ideacrew.com>
2024-04-24 10:11:27 -0500

    revert l10n helper changes

--- app/helpers/l10n_helper.rb
+++ app/helpers/l10n_helper.rb
@@ -1,12 +1,20 @@
 # frozen_string_literal: true
-
+require './lib/html_scrubber_util'
 module L10nHelper
   include ActionView::Helpers::TranslationHelper
   include HtmlScrubberUtil
   def l10n(translation_key, interpolated_keys = {})
     Rails.logger.error {"#L10nHelper passed non string key: #{translation_key.inspect}"} unless translation_key.is_a?(String)
-    return "Translation Missing" unless translation_key.is_a?(String)
-
-    t(translation_key, interpolated_keys.merge(default: translation_key.gsub(/\W+/, '').titleize)).html_safe
+    # https://www.rubydoc.info/github/svenfuchs/i18n/master/I18n%2FBase:translate
+    result = if translation_key.is_a?(String) && interpolated_keys.present?
+               t(
+                 translation_key,
+                 **interpolated_keys.merge(default: translation_key.gsub(/\W+/, '').titleize),
+                 default: translation_key.to_s&.gsub(/\W+/, '')&.titleize
+               )
+             else
+               t(translation_key, default: translation_key.to_s&.gsub(/\W+/, '')&.titleize)
+             end
+    result.respond_to?(:html_safe) ? sanitize_html(result) : translation_key.to_s
   end
 end
