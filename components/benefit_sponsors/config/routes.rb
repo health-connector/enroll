2434a798ca1bed254509199ff6d78c831c4b1e50
utkarsh7989 <utkarsh7989@gmail.com>
2024-05-10 03:54:09 -0400

    updates routes

--- components/benefit_sponsors/config/routes.rb
+++ components/benefit_sponsors/config/routes.rb
@@ -63,9 +63,7 @@ BenefitSponsors::Engine.routes.draw do
   end
 
   namespace :inboxes do
-    resources :messages, only: [:show, :destroy] do
-      get :msg_to_portal
-    end
+    resources :messages, only: [:show, :destroy]
   end
 
   resources :benefit_sponsorships do
