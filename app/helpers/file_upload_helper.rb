7b60d5d0abf65225a5e9923469d72f1634738e1f
Raghu Ram <raghuramg83@gmail.com>
2024-05-24 15:55:35 -0400

    rename helper methods

--- app/helpers/file_upload_helper.rb
+++ app/helpers/file_upload_helper.rb
@@ -2,11 +2,11 @@
 
 # Helper used to validate file uploads
 module FileUploadHelper
-  def valid_file_uploads?(files, content_types)
-    files.all? { |file| valid_file_upload?(file, content_types) }
+  def validate_file_uploads(files, content_types)
+    files.all? { |file| validate_file_upload(file, content_types) }
   end
 
-  def valid_file_upload?(file, content_types)
+  def validate_file_upload(file, content_types)
     file_validator = FileUploadValidator.new(
       file_data: file,
       content_types: content_types
