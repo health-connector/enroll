d710e7ddbde999c5120ad026e64abfa3222b1099
Nisanth Yaganti <yagantinisanth@gmail.com>
2024-06-06 09:35:45 -0400

    adding pundit policies to employers/broker agency controller (#2675)
    * adding pundit policies to employers/broker agency controller
    
    * rubocop fix
    
    ---------
    
    Co-authored-by: Sri Harsha <sriharsha.poosa@gmail.com>

--- components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/employers/broker_agency_controller.rb
+++ components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/employers/broker_agency_controller.rb
@@ -4,9 +4,10 @@ module BenefitSponsors
       class BrokerAgencyController < ::BenefitSponsors::ApplicationController
         before_action :find_employer
         before_action :find_broker_agency, :except => [:index, :active_broker]
-        before_action :updateable?, only: [:create, :terminate]
 
         def index
+          authorize @employer_profile
+
           @filter_criteria = params.permit(:q, :working_hours, :languages => [])
 
           if @filter_criteria.empty?
@@ -36,36 +37,46 @@ module BenefitSponsors
         end
 
         def show
+          authorize @employer_profile
         end
 
         def active_broker
+          authorize @employer_profile
+
           @broker_agency_account = @employer_profile.active_broker_agency_account
         end
 
         def create
-          @broker_management_form = BenefitSponsors::Organizations::OrganizationForms::BrokerManagementForm.for_create(sanitized_params)
-          @broker_management_form.save
-          flash[:notice] = "Your broker has been notified of your selection and should contact you shortly. You can always call or email them directly. If this is not the broker you want to use, select 'Change Broker'."
-          redirect_to profiles_employers_employer_profile_path(@employer_profile, tab: 'brokers')
-        rescue => e
-          error_msgs = @broker_management_form.errors.map(&:full_messages) if @broker_management_form.errors
-          Rails.logger.warn("Unable to create broker. Error Messages: #{error_msgs}, Error: #{e}")
-          redirect_back(fallback_location: main_app.root_path, :flash => {error: error_msgs})
+          authorize @employer_profile
+          begin
+            @broker_management_form = BenefitSponsors::Organizations::OrganizationForms::BrokerManagementForm.for_create(sanitized_params)
+            @broker_management_form.save
+            flash[:notice] = "Your broker has been notified of your selection and should contact you shortly. You can always call or email them directly. If this is not the broker you want to use, select 'Change Broker'."
+            redirect_to profiles_employers_employer_profile_path(@employer_profile, tab: 'brokers')
+          rescue StandardError => e
+            error_msgs = @broker_management_form.errors.map(&:full_messages) if @broker_management_form.errors
+            Rails.logger.warn("Unable to create broker. Error Messages: #{error_msgs}, Error: #{e}")
+            redirect_back(fallback_location: main_app.root_path, :flash => {error: error_msgs})
+          end
         end
 
         def terminate
-          @broker_management_form = BenefitSponsors::Organizations::OrganizationForms::BrokerManagementForm.for_terminate(sanitize_terminate_params)
+          authorize @employer_profile
 
-          if @broker_management_form.terminate && @broker_management_form.direct_terminate
-            flash[:notice] = "Broker terminated successfully."
-            redirect_to profiles_employers_employer_profile_path(@employer_profile, tab: "brokers")
-          else
-            redirect_to profiles_employers_employer_profile_path(@employer_profile)
+          begin
+            @broker_management_form = BenefitSponsors::Organizations::OrganizationForms::BrokerManagementForm.for_terminate(sanitize_terminate_params)
+
+            if @broker_management_form.terminate && @broker_management_form.direct_terminate
+              flash[:notice] = "Broker terminated successfully."
+              redirect_to profiles_employers_employer_profile_path(@employer_profile, tab: "brokers")
+            else
+              redirect_to profiles_employers_employer_profile_path(@employer_profile)
+            end
+          rescue StandardError => e
+            Rails.logger.warn("Unable to terminate broker. Error: #{e}")
+            flash[:error] = "Unable to terminate broker. Please contact customer service at #{Settings.contact_center.phone_number}."
+            redirect_to profiles_employers_employer_profile_path(@employer_profile, tab: 'brokers')
           end
-        rescue StandardError => e
-          Rails.logger.warn("Unable to terminate broker. Error: #{e}")
-          flash[:error] = "Unable to terminate broker. Please contact customer service at #{Settings.contact_center.phone_number}."
-          redirect_to profiles_employers_employer_profile_path(@employer_profile, tab: 'brokers')
         end
 
         private
@@ -78,10 +89,6 @@ module BenefitSponsors
 
         def sanitize_terminate_params
           params.permit(:direct_terminate, :termination_date, :employer_profile_id, :broker_agency_id)
-        end
-
-        def updateable?
-          authorize @employer_profile, :updateable?
         end
 
         def find_employer
