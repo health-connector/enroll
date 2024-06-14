cfab5ae40baf2877dbf18ba8bd16973957f76333
rahul <rahul.gudimetla@ideacrew.com>
2024-06-14 11:27:16 -0500

    update config file to compatible with rails 6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,8 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
+  # Log disallowed deprecations.
+  config.active_support.disallowed_deprecation = :log
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = Logger::SimpleJsonFormatter.new
@@ -98,6 +100,7 @@ Rails.application.configure do
 
   #Queue adapter
   config.active_job.queue_adapter = :resque
+  config.action_mailer.perform_caching = false
 
   Mongoid.logger.level = Logger::ERROR
   Mongo::Logger.logger.level = Logger::ERROR
