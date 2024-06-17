ae8ccb62872a5c8530b284af6b3bd020ec0f510f
nks2109 <nikhilks219@gmail.com>
2024-06-17 12:11:26 -0400

    refactor agent policy to simplify agent? method and add comments

--- app/policies/agent_policy.rb
+++ app/policies/agent_policy.rb
@@ -2,13 +2,6 @@
 
 #AgentPolicy
 class AgentPolicy < ApplicationPolicy
-  attr_accessor :person
-
-  def initialize(user, _record)
-    super
-    @person = user.person
-  end
-
   def home?
     agent?
   end
@@ -29,11 +22,24 @@ class AgentPolicy < ApplicationPolicy
     agent?
   end
 
+  private
+
+  # Checks if the current user has any agent-related role.
+  #
+  # The user is considered an agent if they have any of the following roles:
+  # - CSR
+  # - Assister
+  # - HBX Staff
+  # - Active Broker
+  #
+  # @return [Boolean] Returns true if the user has an agent-related role, false otherwise.
+  # @note This method is used to determine whether the current user is authorized to perform actions related to agent functionalities.
   def agent?
-    return true if person.csr_role
-    return true if person.assister_role
-    return true if person.hbx_staff_role
-    return true if person.broker_role&.active?
+    return false unless account_holder_person
+    return true if account_holder_person.csr_role
+    return true if account_holder_person.assister_role
+    return true if account_holder_person.hbx_staff_role
+    return true if account_holder_person.broker_role&.active?
 
     false
   end
