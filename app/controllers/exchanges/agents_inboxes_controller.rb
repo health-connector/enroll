50a83955b6eae1816ab206a6dc71ed9dca5d0737
sri49 <sriharsha.poosa@gmail.com>
2024-06-21 15:59:14 -0400

    Revert "Merge branch '187554056-agents-inboxes-controller' into enroll_uat"
    This reverts commit aa93a01657b8a824e339cfa00273078b79f5a368, reversing
    changes made to 7d6f7c27146dcdd8bc3f2dae906ca121cd003c0d.

--- app/controllers/exchanges/agents_inboxes_controller.rb
+++ app/controllers/exchanges/agents_inboxes_controller.rb
@@ -1,20 +1,17 @@
 class Exchanges::AgentsInboxesController < InboxesController
   def find_inbox_provider
-    @inbox_provider = current_user.person
-    @inbox_provider_name = "Agent"
+      @inbox_provider = current_user.person
+      @inbox_provider_name = "Agent"
   end
 
   def destroy
-    authorize :agent
-
     @sent_box = true
     super
   end
 
   def show
-    authorize :agent
-
     @sent_box = true
     super
   end
+
 end
--- app/policies/agent_policy.rb
+++ app/policies/agent_policy.rb
@@ -14,10 +14,6 @@ class AgentPolicy < ApplicationPolicy
     agent?
   end
 
-  def destroy?
-    show?
-  end
-
   def begin_employee_enrollment?
     agent?
   end
