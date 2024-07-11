37d2b96b8861d41005c0c51a6093c17058f469a2
rahul <rahul.gudimetla@ideacrew.com>
2024-07-11 09:12:22 -0500

    cleaning up loggers since the issue is tracked

--- components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/registrations_controller.rb
+++ components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/registrations_controller.rb
@@ -29,7 +29,6 @@ module BenefitSponsors
       def create
         @logger = Logger.new("#{Rails.root}/log/registrations_controller.log")
         @agency = BenefitSponsors::Organizations::OrganizationForms::RegistrationForm.for_create(registration_params)
-        @logger.info("*** Registration Params: #{registration_params.inspect} ***")
         authorize @agency
         @logger.info("*** Auth successful***")
         begin
@@ -37,7 +36,6 @@ module BenefitSponsors
           @logger.info("*** Registration Saved: #{saved}, result_url: #{result_url} ***")
           result_url = send(result_url)
           if saved
-            @logger.info("*** is_employer_profile?: #{is_employer_profile?} ***")
             if is_employer_profile?
               person = current_person
               @logger.info("*** creating SSO account user - #{current_user&.id} ***")
@@ -51,11 +49,11 @@ module BenefitSponsors
             return
           end
         rescue Exception => e
-          @logger.info("*** Exception in registrations controller: message: #{e.message} and backtrace: #{e.backtrace&.join("\n")} ***")
+          @logger.error("*** Exception in registrations controller: message: #{e.message} and backtrace: #{e.backtrace&.join("\n")} ***")
           flash[:error] = e.message
         end
         params[:profile_type] = profile_type
-        @logger.info("*** Errors in registrations controller: #{@agency.errors.full_messages} ***")
+        @logger.error("*** Errors in registrations controller: #{@agency.errors.full_messages} ***")
         render default_template, :flash => { :error => @agency.errors.full_messages }
       end
 
