81823b109fd0a8c9bb688f1699ad4289fb134d85
utkarsh7989 <utkarsh7989@gmail.com>
2024-07-18 00:35:26 -0400

    enables strict transport security

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   skip_before_action :require_login
+  before_action :set_cookie_attributes, only: [:index]
 
   def show_hints
     current_user.hints = !current_user.hints
@@ -11,5 +12,12 @@ class WelcomeController < ApplicationController
 
   def form_template
     # created for generic form template access at '/templates/form-template'
+  end
+
+  private
+
+  def set_cookie_attributes
+    response.headers['Set-Cookie'] = "_session_id=#{session.id}; SameSite=Strict; Secure=true; HttpOnly"
+    response.headers['Strict-Transport-Security'] = "max-age=31536000; includeSubDomains; preload"
   end
 end
