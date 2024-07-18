ad0d322f9b082bad3c9d75fecddbc1016fb82acb
utkarsh7989 <utkarsh7989@gmail.com>
2024-07-18 00:35:55 -0400

    adds specs

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -12,6 +12,12 @@ RSpec.describe WelcomeController, :type => :controller do
       it "renders welcome index" do
         expect(response).to render_template("index")
       end
+
+      it "has Cookie attributes" do
+        expect(response.headers["Set-Cookie"]).to match(/SameSite=Strict/)
+        expect(response.headers["Set-Cookie"]).to match(/HttpOnly/)
+        expect(response.headers["Strict-Transport-Security"]).to match(/max-age=31536000; includeSubDomains; preload/)
+      end
     end
 
     context "when not signed in" do
