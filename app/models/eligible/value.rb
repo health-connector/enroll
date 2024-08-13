1cfac4085121e3b33a118e0300885bf80e27ed1e
HArsHA Ellanki <h_vardhan@yahoo.com>
2024-08-13 10:40:59 -0400

    introduce top level eligible:eligibility models

---
+++ app/models/eligible/value.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Eligible
+  # Value model
+  class Value
+    include Mongoid::Document
+    include Mongoid::Timestamps
+
+    field :title, type: String
+    field :description, type: String
+    field :key, type: Symbol
+    field :item, type: String
+
+    validates_presence_of :title, :key
+
+    def run
+      true
+    end
+  end
+end
