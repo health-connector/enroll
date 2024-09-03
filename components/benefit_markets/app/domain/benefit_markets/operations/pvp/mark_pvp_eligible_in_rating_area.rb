e6c13a38f90e904e1fd130ce4645446c5973e071
Harshavardhan Ellanki <h_vardhan@yahoo.com>
2024-09-03 10:40:22 -0400

    script to mark premium value plans (#2750)
    * script to mark premium value plans
    
    * flickering spec fix

---
+++ components/benefit_markets/app/domain/benefit_markets/operations/pvp/mark_pvp_eligible_in_rating_area.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+require 'dry/monads'
+require 'dry/monads/do'
+
+module BenefitMarkets
+  module Operations
+    module Pvp
+      class MarkPvpEligibleInRatingArea
+        send(:include, Dry::Monads[:result, :do])
+
+        def call(params)
+          values = yield validate(params)
+          product = yield get_product(values)
+          rating_area = yield get_rating_area(values)
+          pvp = yield find_or_create_pvp(product, rating_area)
+          pvp_eligibility = yield create_or_update_pvp_eligibilities(pvp, values)
+
+          Success(pvp_eligibility)
+        end
+
+        private
+
+        def validate(params)
+          errors = []
+          errors << "HiosId is missing" unless params[:hios_id].present?
+          errors << "ActiveYear is missing" unless params[:active_year].present?
+          errors << "RatingAreaCode is missing" unless params[:rating_area_code].present?
+          errors << "PvpEligibility is missing" unless params[:evidence_value].present?
+          @user = User.where(email: params[:updated_by]).first
+          errors << "User Not found with email: #{params[:updated_by]}" unless @user.present?
+
+          errors.empty? ? Success(params) : Failure(errors)
+        end
+
+        def get_product(values)
+          products = ::BenefitMarkets::Products::Product.by_year(values[:active_year]).where(
+            hios_id: values[:hios_id]
+          )
+
+          if products.count == 1
+            Success(products.first)
+          else
+            Failure("No or more than 1 products found with hios_id: #{values[:hios_id]} and year #{values[:active_year]}")
+          end
+        end
+
+        def get_rating_area(values)
+          exchange_provided_code = "R-MA#{values[:rating_area_code].to_s.rjust(3, '0')}"
+          rating_areas = ::BenefitMarkets::Locations::RatingArea.where(
+            active_year: values[:active_year],
+            exchange_provided_code: exchange_provided_code
+          )
+
+          if rating_areas.count == 1
+            Success(rating_areas.first)
+          else
+            Failure("No or more than 1 rating_area found with rating_area_code: #{values[:rating_area_code]} and year #{values[:active_year]}")
+          end
+        end
+
+        def find_or_create_pvp(product, rating_area)
+          ::BenefitMarkets::Operations::Pvp::FindOrCreate.new.call(
+            product_id: product.id,
+            rating_area_id: rating_area.id
+          )
+        end
+
+        def create_or_update_pvp_eligibilities(pvp, values)
+          effective_date = values[:effective_date] || TimeKeeper.date_of_record
+          current_eligibility = pvp.latest_active_pvp_eligibility_on(effective_date)
+          return Success(current_eligibility) if current_eligibility.present?.to_s == values[:evidence_value].to_s
+
+          ::BenefitMarkets::Operations::Pvp::CreatePvpEligibility.new.call(
+            subject: pvp.to_global_id,
+            evidence_key: :shop_pvp_evidence,
+            evidence_value: values[:evidence_value].to_s,
+            effective_date: effective_date,
+            current_user: @user.to_global_id
+          )
+        end
+      end
+    end
+  end
+end
---
+++ spec/data_migrations/products/mark_premium_value_products_in_rating_areas_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+require File.join(Rails.root, "app", "data_migrations", "products", "mark_premium_value_products_in_rating_areas")
+
+describe MarkPremiumValueProductsInRatingAreas, dbclean: :after_each do
+  before :all do
+    DatabaseCleaner.clean
+  end
+
+  let(:given_task_name) { "mark_premium_value_products_in_rating_areas" }
+
+  let!(:site)            { FactoryBot.create(:benefit_sponsors_site, :with_benefit_market, :with_benefit_market_catalog_and_product_packages, :as_hbx_profile, Settings.site.key) }
+  let(:effective_date)   { TimeKeeper.date_of_record.beginning_of_year }
+  let(:catalog)          { site.benefit_markets[0].benefit_market_catalogs[0] }
+  let(:product)          { catalog.product_packages[0].products.first }
+  let(:user)             { FactoryBot.create(:user) }
+
+  let!(:rating_area) do
+    r = product.premium_tables.first.rating_area
+    r.update!(exchange_provided_code: "R-MA001")
+    r
+  end
+
+  subject { MarkPremiumValueProductsInRatingAreas.new(given_task_name, double(:current_scope => nil)) }
+
+  let(:file_name) { File.expand_path("#{Rails.root}/spec/test_data/pvp_plans.csv") }
+
+  before do
+    headers = ["ActiveYear", "HiosId", "RatingAreaCode", "PvpEligibility", "UserEmail"]
+    CSV.open(file_name, "w", force_quotes: true) do |csv|
+      csv << headers
+      csv << [product.active_year, product.hios_id, 1, true, user.email]
+    end
+  end
+
+  after :all do
+    file_name = File.expand_path("#{Rails.root}/spec/test_data/pvp_plans.csv")
+    FileUtils.rm_f(file_name)
+    DatabaseCleaner.clean
+  end
+
+  around do |example|
+    ClimateControl.modify file_name: "spec/test_data/pvp_plans.csv" do
+      example.run
+    end
+  end
+
+  it "marks plans as pvp eligible" do
+    expect(BenefitMarkets::Products::PremiumValueProduct.all.count).to eq 0
+    subject.migrate
+    expect(BenefitMarkets::Products::PremiumValueProduct.all.count).to eq 1
+    pvp = BenefitMarkets::Products::PremiumValueProduct.all.first
+    expect(pvp.eligibilities.count).to eq 1
+    expect(pvp.eligibilities.first.eligible?).to be_truthy
+  end
+end
