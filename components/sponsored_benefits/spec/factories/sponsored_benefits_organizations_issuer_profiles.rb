0b355ac3d291471365ae7f1acb73898848a0204b
Hanna <hanna.kalesnikava@ideacrew.com>
2024-09-05 12:16:27 -0700

    Added spec for Carriers table

---
+++ components/sponsored_benefits/spec/factories/sponsored_benefits_organizations_issuer_profiles.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :sponsored_benefits_organizations_issuer_profile, class: 'SponsoredBenefits::Organizations::IssuerProfile' do
+    organization            { build(:benefit_sponsors_organizations_exempt_organization) }
+  end
+end
