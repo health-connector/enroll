1c5d46ea69429a1772be9eff6bf07c1e3c171d40
Hanna <hanna.kalesnikava@ideacrew.com>
2024-09-07 18:40:16 -0700

    Spec for carrie page

--- features/step_definitions/issuer_steps.rb
+++ features/step_definitions/issuer_steps.rb
@@ -53,11 +53,10 @@ And(/^the user visit the Marketplace Plan Year Index page$/) do
   find('a[aria-label="SHOP"]').click
 end
 
-And(/^the user visit the Marketplace Carrier page$/) do
+And(/^the user visit the Marketplace Carriers page$/) do
   year = Time.now.year + 1
   find("a[aria-label='#{year}']").click
 end
-
 
 When(/^the user will see Marketplace Plan Year Index table$/) do
   expect(page).to have_text('Type')
@@ -68,7 +67,7 @@ When(/^the user will see Marketplace Plan Year Index table$/) do
   expect(page).to have_css('.table-responsive .table thead')
 end
 
-When(/^the user will see Marketplace Carrier table$/) do
+When(/^the user will see Marketplace Carriers table$/) do
   expect(page).to have_text('Carrier')
   expect(page).to have_text('Plans')
   expect(page).to have_text('PVP Plans')
@@ -78,6 +77,7 @@ When(/^the user will see Marketplace Carrier table$/) do
 end
 
 Then('the table should have {string} in the {string} column') do |value, column_name|
+  sleep 50
   table = find('.table-responsive table.table-wrapper')
   column_index = table.find('thead tr th', text: column_name, exact_text: true).path.split('/')[-1][/\d+/].to_i
 
