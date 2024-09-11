f35087aa920dce7a4a9ca31a65231255f1de61b3
nks2109 <nikhilks219@gmail.com>
2024-09-11 09:15:37 -0400

    fix rubocop issues

--- spec/views/events/v2/employers/updated.xml.haml_spec.rb
+++ spec/views/events/v2/employers/updated.xml.haml_spec.rb
@@ -215,9 +215,9 @@ RSpec.describe "events/v2/employer/updated.haml.erb", dbclean: :around_each do
       context "non termination case" do
         let!(:renewal_benefit_application) do
           FactoryBot.build(:benefit_sponsors_benefit_application,
-                            :with_benefit_package,
-                            aasm_state: :enrollment_eligible,
-                            benefit_sponsorship: employer_profile.active_benefit_sponsorship)
+                           :with_benefit_package,
+                           aasm_state: :enrollment_eligible,
+                           benefit_sponsorship: employer_profile.active_benefit_sponsorship)
         end
         let(:renewal_benefit_package){ renewal_benefit_application.benefit_packages.first}
         let!(:update_renewal) do
