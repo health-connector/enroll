fd7063953e35bf03aac07423cf5fc1b0a60879cb
nks2109 <nikhilks219@gmail.com>
2024-10-07 12:59:33 -0400

    refactor download v2 xml method

--- components/benefit_sponsors/app/models/benefit_sponsors/services/group_xml_downloader.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/services/group_xml_downloader.rb
@@ -14,16 +14,25 @@ module BenefitSponsors
 
       def download(controller)
         carrier_files = employer_event.render_payloads
-        z_file = Tempfile.new("employer_events_digest")
-        zip_path = "#{z_file.path}.zip"
-        z_file.close
-        z_file.unlink
+        empty_files = carrier_files.select { |car| car.instance_variable_get(:@rendered_employers).empty? }
+
+        return :empty_files if empty_files.size == carrier_files.size
+
+        a_time = Time.now
+        today_date_time = TimeKeeper.local_time(a_time).strftime("%Y%m%d_%H%M%S")
+        zip_file_name = "employer_events_digest_#{today_date_time}.zip"
+        zip_path = Rails.root.join('tmp', zip_file_name)
+        Rails.logger.info "Temporary zip file created at path: #{zip_path}"
+
         ::Zip::File.open(zip_path, ::Zip::File::CREATE) do |zip|
           carrier_files.each do |car|
-            car.write_to_zip(zip)
+            car.write_to_zip(zip) unless car.instance_variable_get(:@rendered_employers).empty?
           end
         end
-        controller.send_file(zip_path)
+
+        controller.send_file(zip_path, filename: zip_file_name, type: 'application/zip', disposition: 'attachment')
+        File.delete(zip_path)
+        :success
       end
     end
   end
