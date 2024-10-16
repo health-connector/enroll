f3a3cc5fb335dc0a7c6fcc0f82beff9fff62b876
Hanna <hanna.kalesnikava@ideacrew.com>
2024-10-16 16:08:24 -0700

    remove set_default_ga action and endpoint

--- app/controllers/broker_agencies/profiles_controller.rb
+++ app/controllers/broker_agencies/profiles_controller.rb
@@ -15,32 +15,6 @@ class BrokerAgencies::ProfilesController < ApplicationController
     "5"     => "employer_profile.plan_years.start_on"
   }
 
-  def set_default_ga
-    authorize @broker_agency_profile, :set_default_ga?
-    @general_agency_profile = GeneralAgencyProfile.find(params[:general_agency_profile_id]) rescue nil
-    if @broker_agency_profile.present?
-      old_default_ga_id = @broker_agency_profile.default_general_agency_profile.id.to_s rescue nil
-      if params[:type] == 'clear'
-        @broker_agency_profile.default_general_agency_profile = nil
-      elsif @general_agency_profile.present?
-        @broker_agency_profile.default_general_agency_profile = @general_agency_profile
-        @broker_agency_profile.employer_clients.each do |employer_profile|
-          @general_agency_profile.general_agency_hired_notice(employer_profile) # GA notice when broker selects a default GA
-        end
-      end
-      @broker_agency_profile.save
-      notify("acapi.info.events.broker.default_ga_changed", {:broker_id => @broker_agency_profile.primary_broker_role.hbx_id, :pre_default_ga_id => old_default_ga_id})
-      @notice = "Changing default general agencies may take a few minutes to update all employers."
-
-      @broker_role = current_user.person.broker_role || nil
-      @general_agency_profiles = GeneralAgencyProfile.all_by_broker_role(@broker_role, approved_only: true)
-    end
-
-    respond_to do |format|
-      format.js
-    end
-  end
-
   def assign
 
     page_string = params.permit(:employers_page)[:employers_page]
