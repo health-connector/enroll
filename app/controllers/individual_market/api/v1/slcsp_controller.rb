238ce2553926ca582a3ba4ef176eaa41c995c15a
Hanna <hanna.kalesnikava@ideacrew.com>
2024-10-21 08:46:15 -0700

    fix rubocop issues

--- app/controllers/individual_market/api/v1/slcsp_controller.rb
+++ app/controllers/individual_market/api/v1/slcsp_controller.rb
@@ -6,28 +6,29 @@ module IndividualMarket
       class SlcspController < ActionController::Base
 
         def plan
-          begin
-            request_xml = request.body.read
-            parsed_request = HappyMapper.parse(request_xml)
-            @plan = find_slcsp(Date::strptime(parsed_request.coverage_start, "%Y%m%d"))
+          request_xml = request.body.read
+          parsed_request = HappyMapper.parse(request_xml)
+          @plan = find_slcsp(Date.strptime(parsed_request.coverage_start, "%Y%m%d"))
 
-            render :template => 'shared/_plan.xml.builder', :layout => false, :status => :ok
-          rescue Exception => e
-            render :xml => "<errors><error>#{e.message}</error></errors>", :status => :unprocessable_entity
-          end
+          render :template => 'shared/_plan.xml.builder', :layout => false, status: :ok
+
+        # rubocop:disable Lint/RescueException
+        rescue Exception => e
+          render :xml => "<errors><error>#{e.message}</error></errors>", status: :unprocessable_entity
         end
 
         private
 
         def find_slcsp(coverage_start)
-
-          benefit_coverage_period = Organization.where(dba:'DCHL').first.hbx_profile.benefit_sponsorship.benefit_coverage_periods.detect do |benefit_coverage_period|
-            benefit_coverage_period.start_on <= coverage_start && benefit_coverage_period.end_on >= coverage_start
+          benefit_coverage_period = Organization
+                                    .where(dba: 'DCHL')
+                                    .first.hbx_profile
+                                    .benefit_sponsorship.benefit_coverage_periods
+                                    .detect do |bcp|
+            bcp.start_on <= coverage_start && benefit_coverage_period.end_on >= coverage_start
           end
 
-          raise "SLCSP could not be found" if benefit_coverage_period.nil?
-
-          Plan.find(benefit_coverage_period.slcsp) rescue "SLCSP could not be found"
+          Plan.find(benefit_coverage_period.slcsp) unless benefit_coverage_period.nil?
         end
       end
     end
