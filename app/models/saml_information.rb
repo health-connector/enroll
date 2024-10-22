53f63a97b819f912ebcdfb8d9ec0109f9fba355e
Brian Henry <brian.henry@ideacrew.com>
2024-10-22 10:53:15 -0600

    fix bad method call

--- app/models/saml_information.rb
+++ app/models/saml_information.rb
@@ -67,6 +67,6 @@ class SamlInformation
   # development, better that we allow less config setup work.
   def val_or_nonprod_default(val, nonprod_default)
     return val if val.present?
-    nonprod_default unless Rails.env.production
+    nonprod_default unless Rails.env.production?
   end
 end
