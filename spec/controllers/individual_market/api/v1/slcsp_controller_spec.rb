627183038580e73bfdf8e40ab0df8b4249850d2e
hKalesnikavaideacrew <161342696+hKalesnikavaideacrew@users.noreply.github.com>
2024-11-06 08:15:39 -0800

    unmount Slcsp controller (#2816)
    * unmount Slcsp controller
    
    * move Slcsp routes
    
    * move Slcsp controller
    
    * fix rubocop issues
    
    * fix rubocop issue
    
    * master merge
    
    ---------
    
    Co-authored-by: Ryan Eddy <44847768+RyanEddyIC@users.noreply.github.com>
    Co-authored-by: Sri Harsha <sriharsha.poosa@gmail.com>

--- spec/controllers/individual_market/api/v1/slcsp_controller_spec.rb
+++ spec/controllers/individual_market/api/v1/slcsp_controller_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-describe Api::V1::SlcspController do
+describe IndividualMarket::Api::V1::SlcspController, skip: true do
 
   let(:request_xml) { File.read(Rails.root.join("spec", "test_data", "slcsp_payloads", "request.xml")) }
   let(:response_xml) { File.read(Rails.root.join("spec", "test_data", "slcsp_payloads", "response.xml")) }
@@ -11,7 +11,7 @@ describe Api::V1::SlcspController do
   context "valid request" do
     it 'returns https status 200' do
       allow(HappyMapper).to receive(:parse).with(anything).and_return(HappyMapper.parse(request_xml))
-      allow_any_instance_of(Api::V1::SlcspController).to receive(:find_slcsp).with(anything).and_return(plan)
+      allow_any_instance_of(IndividualMarket::Api::V1::SlcspController).to receive(:find_slcsp).with(anything).and_return(plan)
       allow_any_instance_of(ActionController::Rendering).to receive(:render).and_return(response_xml)
 
       post :plan, :format => "xml"
@@ -23,11 +23,10 @@ describe Api::V1::SlcspController do
   context "invalid request" do
     it 'returns https status 422' do
       allow(HappyMapper).to receive(:parse).with(anything).and_raise(Exception.new)
-      allow_any_instance_of(Api::V1::SlcspController).to receive(:find_slcsp).with(anything).and_return(plan)
+      allow_any_instance_of(IndividualMarket::Api::V1::SlcspController).to receive(:find_slcsp).with(anything).and_return(plan)
 
       post :plan, params: { format: :xml }
       expect(response.status).to eq(422)
     end
   end
-
 end
