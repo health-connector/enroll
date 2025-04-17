585dea34cc4f1bec17a3859407c2398a5eb3de24
utkarsh7989 <utkarsh7989@gmail.com>
2025-04-17 00:43:56 -0400

    quick fix

--- app/interactors/persist_enrollment_and_assign_to_assignments.rb
+++ app/interactors/persist_enrollment_and_assign_to_assignments.rb
@@ -21,5 +21,6 @@ class PersistEnrollmentAndAssignToAssignments
   def find_assignment(enrollment)
     assignment = context.employee_role.census_employee.benefit_group_assignment_by_package(enrollment.sponsored_benefit_package_id, enrollment.effective_on)
     context.fail!(message: "No assignment found for effective date #{enrollment.effective_on}") unless assignment.present?
+    assignment
   end
 end
