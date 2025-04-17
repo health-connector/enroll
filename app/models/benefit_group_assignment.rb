3caccf00231559cb302a53bffa5fae82ceccf17e
utkarsh7989 <utkarsh7989@gmail.com>
2025-04-17 16:20:57 -0400

    adds specs

--- app/models/benefit_group_assignment.rb
+++ app/models/benefit_group_assignment.rb
@@ -420,13 +420,10 @@ class BenefitGroupAssignment
     return false if start_on.blank? || canceled?
 
     end_date = end_on || start_on.next_year.prev_day
-    Rails.logger.info "start_on: #{start_on} end_date: #{end_date} date: #{date}"
-    Rails.logger.info "benefit_application.start_on: #{benefit_application&.start_on} benefit_application.end_on: #{benefit_application&.end_on}"
-
     # if the benefit application is enrolling, then the start_on date should be the start_on date of the benefit application
-    start_on = benefit_application.start_on if benefit_application&.is_enrolling?
+    start_date = benefit_application&.is_enrolling? ? benefit_application.start_on : start_on
 
-    (start_on..end_date).cover?(date)
+    (start_date..end_date).cover?(date)
   end
 
   def make_active
