206c0e624e2db390d90a398e630c9b4b090de6ca
Hanna <hanna.kalesnikava@ideacrew.com>
2025-05-19 10:52:00 -0700

    remove bearer ignore setting

--- app/reports/hbx_reports/edi_enrollment_termination_report.rb
+++ app/reports/hbx_reports/edi_enrollment_termination_report.rb
@@ -47,7 +47,6 @@ module HbxReports
               primary_person = family.primary_family_member.person
               employer = enrollment.try(:employer_profile)
               census_employee = person.try(:employee_roles).try(:first).try(:census_employee)
-              # bearer:ignore ruby_lang_file_generation
               csv << [
                   person.hbx_id,
                   person.first_name,
