1ee0b618797997e88d4a415573e2639e1d23433a
Hanna <hanna.kalesnikava@ideacrew.com>
2025-06-18 13:16:15 -0700

    fix spec for OrganizationObserver

--- components/benefit_sponsors/spec/models/benefit_sponsors/observers/organization_observer_spec.rb
+++ components/benefit_sponsors/spec/models/benefit_sponsors/observers/organization_observer_spec.rb
@@ -7,16 +7,23 @@ module BenefitSponsors
     RSpec.describe OrganizationObserver, type: :model, dbclean: :after_each do
       let!(:organization) { FactoryBot.create(:benefit_sponsors_organizations_general_organization, :with_site, :with_aca_shop_cca_employer_profile) }
       let(:subject) { BenefitSponsors::Organizations::Organization }
-      let(:observer) { OrganizationObserver.instance }
+      let(:observer) { OrganizationObserver.new }
+
+      before(:each) do
+        allow(observer).to receive(:notify)
+      end
 
       context 'organization legal_name changed' do
         it 'should send notification' do
-          allow(observer).to receive(:notify)
           expected_payload = { employer_id: organization.hbx_id, event_name: "name_changed" }
 
           organization.update_attributes(legal_name: "test")
+          observer.update(organization)
+
           BenefitSponsors::Organizations::Organization::FIELD_AND_EVENT_NAMES_MAP.each_key do |key|
-            expect(observer).to have_received(:notify).with("acapi.info.events.employer.name_changed", expected_payload) if organization.attribute_changed?(key)
+            if organization.saved_change_to_attribute(key)
+              expect(observer).to have_received(:notify).with("acapi.info.events.employer.name_changed", expected_payload)
+            end
           end
         end
 
@@ -38,6 +45,7 @@ module BenefitSponsors
           expected_payload = { employer_id: organization.hbx_id, event_name: "fein_corrected" }
 
           organization.update_attributes(fein: "123456789")
+          observer.update(organization)
           expect(observer).to have_received(:notify).with("acapi.info.events.employer.fein_corrected", expected_payload)
         end
       end
@@ -45,22 +53,22 @@ module BenefitSponsors
       describe '.update' do
         context 'has to send notification when' do
           it 'fein updated' do
-            allow_any_instance_of(OrganizationObserver).to receive(:notify)
-            organization.assign_attributes(fein: "987654532")
+            organization.update_attributes(fein: "987654532")
             observer.update(organization, nil)
             expect(observer).to have_received(:notify).with('acapi.info.events.employer.fein_corrected', {:employer_id => organization.hbx_id, :event_name => "fein_corrected"})
           end
 
           it 'DBA updated' do
-            allow_any_instance_of(OrganizationObserver).to receive(:notify)
-            organization.assign_attributes(dba: "test")
+            organization.update_attributes(dba: "test")
             observer.update(organization, nil)
-            expect(observer).to have_received(:notify).with('acapi.info.events.employer.name_changed', {:employer_id => organization.hbx_id, :event_name => "name_changed"})
+            expect(observer).to have_received(:notify).with(
+              'acapi.info.events.employer.name_changed',
+              { employer_id: organization.hbx_id, event_name: "name_changed" }
+            ).exactly(1).time
           end
 
           it 'legal_name updated' do
-            allow_any_instance_of(OrganizationObserver).to receive(:notify)
-            organization.assign_attributes(legal_name: "test")
+            organization.update_attributes(legal_name: "test")
             observer.update(organization, nil)
             expect(observer).to have_received(:notify).with('acapi.info.events.employer.name_changed', {:employer_id => organization.hbx_id, :event_name => "name_changed"})
           end
