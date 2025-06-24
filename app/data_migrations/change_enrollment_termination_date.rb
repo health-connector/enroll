68f12f9a3dad1bb1c13c142e11b71d1fd38664f4
Hanna <hanna.kalesnikava@ideacrew.com>
2025-06-23 20:49:38 -0700

    rubocop fixes for data migrations

--- app/data_migrations/change_enrollment_termination_date.rb
+++ app/data_migrations/change_enrollment_termination_date.rb
@@ -1,16 +1,15 @@
+# frozen_string_literal: true
 
 require File.join(Rails.root, "lib/mongoid_migration_task")
 
 class ChangeEnrollmentTerminationDate < MongoidMigrationTask
   def migrate
     enrollment = HbxEnrollment.by_hbx_id(ENV['hbx_id'].to_s)
-    new_termination_date = Date.strptime(ENV['termination_date'],'%Y-%m-%d').to_date
+    new_termination_date = Date.strptime(ENV.fetch('termination_date', nil),'%Y-%m-%d').to_date
     enrollment.first.update_attributes(terminated_on: new_termination_date)
     enrollment_members = enrollment.first.hbx_enrollment_members
-    unless enrollment_members.nil?
-      enrollment_members.each do |member|
-        member.update_attributes(coverage_end_on:new_termination_date)
-      end
+    enrollment_members&.each do |member|
+      member.update_attributes(coverage_end_on: new_termination_date)
     end
     puts "Changed Enrollment termination date to #{new_termination_date}" unless Rails.env.test?
   end
