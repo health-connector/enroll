0e3f69817493aec361fd57273e778684a418fdf4
Hanna <hanna.kalesnikava@ideacrew.com>
2025-11-24 08:20:24 -0800

    [116818] update assets config

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,5 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+//= link sponsored_benefits/application.css
+//= link sponsored_benefits/application.js
