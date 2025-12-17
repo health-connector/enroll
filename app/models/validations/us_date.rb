dbc37b40fa0c5286ddddbbeea857de97e0fb2b9b
jayreddy519 <jayanth.rordev@gmail.com>
2025-12-17 11:47:17 -0500

    117157-date-validation brakeman issue

--- app/models/validations/us_date.rb
+++ app/models/validations/us_date.rb
@@ -1,30 +1,31 @@
+# frozen_string_literal: true
+
 module Validations
   class UsDate
-    def self.on(prop_name, allow_blank = false)
-      mod = Module.new
-      mod.define_singleton_method :included do |klass|
-        # rubocop:disable Style/EvalWithLocation, Style/DocumentDynamicEvalDefinition
-        klass.class_eval(<<-RUBYCODE)
-          def __valid_US_date_property_#{prop_name}
-            d_value = #{prop_name}
+    def self.on(prop_name, allow_blank: false)
+      Module.new do
+        define_singleton_method :included do |klass|
+
+          method_name = :"__valid_US_date_property_#{prop_name}"
+
+          klass.define_method(method_name) do
+            d_value = send(prop_name)
+
             begin
               Date.strptime(d_value, "%m/%d/%Y")
-            rescue
-              errors.add(:#{prop_name}, "invalid date: " + d_value.to_s)
+            rescue StandardError
+              errors.add(prop_name, "invalid date: #{d_value}")
             end
           end
-        RUBYCODE
-        # rubocop:enable Style/EvalWithLocation, Style/DocumentDynamicEvalDefinition
-        klass.class_eval do
-          validate "__valid_US_date_property_#{prop_name}".to_sym # rubocop:disable Lint/SymbolConversion
-        end
-        unless allow_blank
-          klass.class_eval do
-            validates_presence_of prop_name.to_sym
-          end
+
+          klass.validate method_name
+
+          return if allow_blank
+
+          klass.validates_presence_of prop_name
+
         end
       end
-      mod
     end
   end
 end
