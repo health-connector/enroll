609cb701e68fc95401d2f8fb53ec93aeee9f40d1
Hanna <hanna.kalesnikava@ideacrew.com>
2026-02-19 14:00:40 -0800

    rubocop fix for OrganizationConcern

--- components/sponsored_benefits/app/models/sponsored_benefits/concerns/organization_concern.rb
+++ components/sponsored_benefits/app/models/sponsored_benefits/concerns/organization_concern.rb
@@ -3,129 +3,128 @@
 require 'active_support/concern'
 
 module SponsoredBenefits
-  module Concerns
-    module OrganizationConcern
-      extend ActiveSupport::Concern
+  module Concerns::OrganizationConcern
+    extend ActiveSupport::Concern
 
-      included do
-        include Mongoid::Document
-        include Mongoid::Timestamps
+    included do
+      include Mongoid::Document
+      include Mongoid::Timestamps
 
-        field :hbx_id, type: String
+      field :hbx_id, type: String
 
-        # Registered legal name
-        field :legal_name, type: String
+      # Registered legal name
+      field :legal_name, type: String
 
-        # Doing Business As (alternate name)
-        field :dba, type: String
+      # Doing Business As (alternate name)
+      field :dba, type: String
 
-        # Federal Employer ID Number
-        field :fein, type: String
+      # Federal Employer ID Number
+      field :fein, type: String
 
-        field :entity_kind, type: String
+      field :entity_kind, type: String
 
-        # Web URL
-        field :home_page, type: String
+      # Web URL
+      field :home_page, type: String
 
-        field :is_active, type: Mongoid::Boolean
+      field :is_active, type: Mongoid::Boolean
 
-        field :is_fake_fein, type: Mongoid::Boolean
+      field :is_fake_fein, type: Mongoid::Boolean
 
-        # User or Person ID who created/updated
-        field :updated_by, type: BSON::ObjectId
+      # User or Person ID who created/updated
+      field :updated_by, type: BSON::ObjectId
 
-        embeds_many :office_locations, class_name: "SponsoredBenefits::Organizations::OfficeLocation", cascade_callbacks: true, validate: true
+      embeds_many :office_locations, class_name: "SponsoredBenefits::Organizations::OfficeLocation", cascade_callbacks: true, validate: true
 
-        accepts_nested_attributes_for :office_locations, class_name: "SponsoredBenefits::Organizations::OfficeLocation", allow_destroy: true
+      accepts_nested_attributes_for :office_locations, class_name: "SponsoredBenefits::Organizations::OfficeLocation", allow_destroy: true
 
-        validates_presence_of :legal_name, :office_locations
+      validates_presence_of :legal_name, :office_locations
 
-        # validates :fein,
-        #   presence: false,
-        #   length: { is: 9, message: "%{value} is not a valid FEIN" },
-        #   numericality: true,
-        #   uniqueness: true
+      # validates :fein,
+      #   presence: false,
+      #   length: { is: 9, message: "%{value} is not a valid FEIN" },
+      #   numericality: true,
+      #   uniqueness: true
 
-        validate :office_location_kinds
+      validate :office_location_kinds
 
-      index({ hbx_id: 1 })
-      index({ legal_name: 1 })
-      index({ dba: 1 }, {sparse: true})
-      index({ fein: 1 })
-      index({ is_active: 1 })
+    index({ hbx_id: 1 })
+    index({ legal_name: 1 })
+    index({ dba: 1 }, {sparse: true})
+    index({ fein: 1 })
+    index({ is_active: 1 })
 
-        before_save :generate_hbx_id
-        # Commenting out as we implemented feature to update legal name on plan design organization after updating ER legal name.
-        # This will trigger a duplicate notification. And this is the code copied over while moving engines and never got refactored.
-        # after_update :legal_name_or_fein_change_attributes,:if => :check_legal_name_or_fein_changed?
+      before_save :generate_hbx_id
+      # Commenting out as we implemented feature to update legal name on plan design organization after updating ER legal name.
+      # This will trigger a duplicate notification. And this is the code copied over while moving engines and never got refactored.
+      # after_update :legal_name_or_fein_change_attributes,:if => :check_legal_name_or_fein_changed?
 
-        def generate_hbx_id
-          write_attribute(:hbx_id, SponsoredBenefits::Organizations::HbxIdGenerator.generate_organization_id) if hbx_id.blank?
-        end
+      def generate_hbx_id
+        write_attribute(:hbx_id, SponsoredBenefits::Organizations::HbxIdGenerator.generate_organization_id) if hbx_id.blank?
+      end
 
-        def office_location_kinds
-          location_kinds = office_locations.reject(&:marked_for_destruction?).flat_map(&:address).compact.flat_map(&:kind)
-          # should validate only office location which are not persisted AND kinds ie. primary, mailing, branch
-          return if location_kinds.detect {|kind| ['work', 'home'].include?(kind)}
+      def office_location_kinds
+        location_kinds = office_locations.reject(&:marked_for_destruction?).flat_map(&:address).compact.flat_map(&:kind)
+        # should validate only office location which are not persisted AND kinds ie. primary, mailing, branch
+        return if location_kinds.detect {|kind| ['work', 'home'].include?(kind)}
 
-          if location_kinds.count('primary').zero?
-            errors.add(:base, "must select one primary address")
-          elsif location_kinds.count('primary') > 1
-            errors.add(:base, "can't have multiple primary addresses")
-          elsif location_kinds.count('mailing') > 1
-            errors.add(:base, "can't have more than one mailing address")
-          end
-        end
-
-        def check_legal_name_or_fein_changed?
-          fein_previously_changed? || legal_name_previously_changed?
-        end
-
-        def legal_name_or_fein_change_attributes
-          @changed_fields = changed_attributes.keys
-          notify_legal_name_or_fein_change if changed_attributes.keys.include?("fein")
-        end
-
-        def notify_legal_name_or_fein_change
-          return unless employer_profile.present?
-
-          FIELD_AND_EVENT_NAMES_MAP.each do |feild, event_name|
-            notify("acapi.info.events.employer.#{event_name}", {employer_id: hbx_id, event_name: event_name.to_s}) if @changed_fields.present? && @changed_fields.include?(feild)
-          end
-        end
-
-        def office_location_county
-          primary_office_location.county
-        end
-
-        def office_location_zip
-          primary_office_location.zip
-        end
-
-        def primary_office_location
-          office_locations.detect(&:is_primary?)
+        if location_kinds.count('primary').zero?
+          errors.add(:base, "must select one primary address")
+        elsif location_kinds.count('primary') > 1
+          errors.add(:base, "can't have multiple primary addresses")
+        elsif location_kinds.count('mailing') > 1
+          errors.add(:base, "can't have more than one mailing address")
         end
       end
 
-      # rubocop:disable Lint/ConstantDefinitionInBlock
-      class_methods do
-        PROFILE_KINDS = [:plan_design_profile, :employer_profile, :broker_agency_profile].freeze
-        ENTITY_KINDS = [
-          "tax_exempt_organization",
-          "c_corporation",
-          "s_corporation",
-          "sole_proprietor",
-          "partnership",
-          "limited_liability_corporation",
-          "limited_liability_partnership",
-          "household_employer",
-          "governmental_employer",
-          "foreign_embassy_or_consulate"
-        ].freeze
-
-        FIELD_AND_EVENT_NAMES_MAP = {"legal_name" => "name_changed", "fein" => "fein_corrected"}.freeze
+      def check_legal_name_or_fein_changed?
+        fein_previously_changed? || legal_name_previously_changed?
       end
-      # rubocop:enable Lint/ConstantDefinitionInBlock
+
+      def legal_name_or_fein_change_attributes
+        @changed_fields = changed_attributes.keys
+        notify_legal_name_or_fein_change if changed_attributes.keys.include?("fein")
+      end
+
+      def notify_legal_name_or_fein_change
+        return unless employer_profile.present?
+
+        FIELD_AND_EVENT_NAMES_MAP.each do |feild, event_name|
+          notify("acapi.info.events.employer.#{event_name}", {employer_id: hbx_id, event_name: event_name.to_s}) if @changed_fields.present? && @changed_fields.include?(feild)
+        end
+      end
+
+      def office_location_county
+        primary_office_location.county
+      end
+
+      def office_location_zip
+        primary_office_location.zip
+      end
+
+      def primary_office_location
+        office_locations.detect(&:is_primary?)
+      end
     end
+
+    # rubocop:disable Lint/ConstantDefinitionInBlock
+    class_methods do
+      PROFILE_KINDS = [:plan_design_profile, :employer_profile, :broker_agency_profile].freeze
+      ENTITY_KINDS = [
+        "tax_exempt_organization",
+        "c_corporation",
+        "s_corporation",
+        "sole_proprietor",
+        "partnership",
+        "limited_liability_corporation",
+        "limited_liability_partnership",
+        "household_employer",
+        "governmental_employer",
+        "foreign_embassy_or_consulate"
+      ].freeze
+
+      FIELD_AND_EVENT_NAMES_MAP = {"legal_name" => "name_changed", "fein" => "fein_corrected"}.freeze
+    end
+    # rubocop:enable Lint/ConstantDefinitionInBlock
+
   end
 end
