99b573a3d8acf2d23208e0f5dfb72b8d5f0032e1
Charlie Parker <charlie.parker@ideacrew.com>
2026-03-30 13:30:43 -0400

    global updates for contact info and copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -47,11 +47,7 @@ module ApplicationHelper
   end
 
   def copyright_notice
-    if TimeKeeper.date_of_record.year.to_s == Settings.site.copyright_period_start.to_s
-      copyright_attribution = "#{Settings.site.copyright_period_start} #{Settings.site.long_name}"
-    else
-      copyright_attribution = "#{Settings.site.copyright_period_start}-#{TimeKeeper.date_of_record.year} #{Settings.site.long_name}"
-    end
+    copyright_attribution = "#{copyright_period_start} #{Settings.site.long_name}"
     raw("<span class='copyright'><i class='far fa-copyright fa-lg' aria-hidden='true'></i> #{copyright_attribution}. All Rights Reserved. </span>")
   end
 
