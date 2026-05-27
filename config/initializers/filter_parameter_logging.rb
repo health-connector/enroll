6523fb5301dac20e8eacf48591f98e34693c47f3
Utkarsh Shukla <utkarsh.shukla@ideacrew.com>
2026-05-27 12:57:41 -0400

    sanitize DOB-related parameters

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :question_answer, :password_confirmation, :new_password, :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :employer_attestation_id, :employers_action_id,
-                                               :employer_actions_id, :family_actions_id]
+Rails.application.config.filter_parameters += [:password, :question_answer, :password_confirmation, :new_password, :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :dob, :new_dob, :date_of_birth, :birth_date,
+                                               :employer_attestation_id, :employers_action_id, :employer_actions_id, :family_actions_id]
