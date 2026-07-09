69271610776afcb5ff5d1ab1fc5effb24878e0fd
nks2109 <nikhilks219@gmail.com>
2026-07-09 09:29:09 -0400

    remove puts statements

--- app/models/time_keeper.rb
+++ app/models/time_keeper.rb
@@ -49,7 +49,6 @@ class TimeKeeper
   def self.set_date_of_record(new_date)
     new_date = new_date.to_date
     Rails.logger.info("[TimeKeeper] set_date_of_record called: stored=#{instance.date_of_record} requested=#{new_date}")
-    puts "[TimeKeeper] set_date_of_record called: stored=#{instance.date_of_record} requested=#{new_date}"
     if instance.date_of_record != new_date
       if instance.date_of_record > new_date
         log("Attempt made to set date to past: #{new_date}", {:severity => :error})
@@ -88,7 +87,6 @@ class TimeKeeper
   def set_date_of_record(new_date)
     Rails.cache.write(CACHE_KEY, new_date.strftime("%Y-%m-%d"))
     Rails.logger.info("[TimeKeeper] cache write: #{CACHE_KEY} = #{new_date.strftime('%Y-%m-%d')}")
-    puts "[TimeKeeper] cache write: #{CACHE_KEY} = #{new_date.strftime('%Y-%m-%d')}"
   end
 
   def date_of_record
@@ -96,7 +94,6 @@ class TimeKeeper
     return tl_value unless tl_value.blank?
     found_value = Rails.cache.fetch(CACHE_KEY) do
       Rails.logger.info("[TimeKeeper] cache miss for #{self.class::CACHE_KEY} - falling back to Date.current (#{Date.current})")
-      puts "[TimeKeeper] cache miss for #{self.class::CACHE_KEY} - falling back to Date.current (#{Date.current})"
       log("date_of_record not available for TimeKeeper - using Date.current")
       Date.current.strftime("%Y-%m-%d")
     end
@@ -106,7 +103,6 @@ class TimeKeeper
   def push_date_of_record
     notify_logger("TimeKeeper advance day started at #{Time.now.in_time_zone('Eastern Time (US & Canada)').strftime('%m-%d-%Y %H:%M:%S')}")
     Rails.logger.info("[TimeKeeper] calling AcaShopScheduledEvents.advance_day for date=#{date_of_record}")
-    puts "[TimeKeeper] calling AcaShopScheduledEvents.advance_day for date=#{date_of_record}"
     Rails.logger.info("[TimeKeeper] ------- step 1 ----------- | Date.today=#{Date.today} | TimeKeeper.date_of_record=#{TimeKeeper.date_of_record}")
     BenefitSponsors::ScheduledEvents::AcaShopScheduledEvents.advance_day(self.date_of_record)
     Rails.logger.info("[TimeKeeper] ------- step 2 ----------- | Date.today=#{Date.today} | TimeKeeper.date_of_record=#{TimeKeeper.date_of_record}")
--- components/benefit_sponsors/app/models/benefit_sponsors/scheduled_events/aca_shop_scheduled_events.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/scheduled_events/aca_shop_scheduled_events.rb
@@ -15,7 +15,6 @@ module BenefitSponsors
         @new_date = new_date
         initialize_logger
         Rails.logger.info("[AcaShopScheduledEvents] advance_day processing date: #{new_date} | Date.today=#{Date.today} | TimeKeeper.date_of_record=#{TimeKeeper.date_of_record}")
-        puts "[AcaShopScheduledEvents] advance_day processing date: #{new_date} | Date.today=#{Date.today} | TimeKeeper.date_of_record=#{TimeKeeper.date_of_record}"
         shop_daily_events
         process_events_for('auto_submit_renewal_applications') { auto_submit_renewal_applications }
         # process_events_for('process_applications_missing_binder_payment') { process_applications_missing_binder_payment } #refs 39124 - Had to comment out as we got rid of states on BS.
@@ -37,35 +36,30 @@ module BenefitSponsors
       def open_enrollment_begin
         benefit_sponsorships = BenefitSponsorships::BenefitSponsorship.may_begin_open_enrollment?(new_date)
         Rails.logger.info("[AcaShopScheduledEvents] open_enrollment_begin: sponsorships_found=#{benefit_sponsorships.count} | Date.today=#{Date.today} | TimeKeeper.date_of_record=#{TimeKeeper.date_of_record}")
-        puts "[AcaShopScheduledEvents] open_enrollment_begin: sponsorships_found=#{benefit_sponsorships.count} | Date.today=#{Date.today} | TimeKeeper.date_of_record=#{TimeKeeper.date_of_record}"
         execute_sponsor_event(benefit_sponsorships, :begin_open_enrollment)
       end
 
       def open_enrollment_end
         benefit_sponsorships = BenefitSponsorships::BenefitSponsorship.may_end_open_enrollment?(new_date)
         Rails.logger.info("[AcaShopScheduledEvents] open_enrollment_end: sponsorships_found=#{benefit_sponsorships.count} | Date.today=#{Date.today} | TimeKeeper.date_of_record=#{TimeKeeper.date_of_record}")
-        puts "[AcaShopScheduledEvents] open_enrollment_end: sponsorships_found=#{benefit_sponsorships.count} | Date.today=#{Date.today} | TimeKeeper.date_of_record=#{TimeKeeper.date_of_record}"
         execute_sponsor_event(benefit_sponsorships, :end_open_enrollment)
       end
 
       def benefit_begin
         benefit_sponsorships = BenefitSponsorships::BenefitSponsorship.may_begin_benefit_coverage?(new_date)
         Rails.logger.info("[AcaShopScheduledEvents] benefit_begin: sponsorships_found=#{benefit_sponsorships.count} | Date.today=#{Date.today} | TimeKeeper.date_of_record=#{TimeKeeper.date_of_record}")
-        puts "[AcaShopScheduledEvents] benefit_begin: sponsorships_found=#{benefit_sponsorships.count} | Date.today=#{Date.today} | TimeKeeper.date_of_record=#{TimeKeeper.date_of_record}"
         execute_sponsor_event(benefit_sponsorships, :begin_sponsor_benefit)
       end
 
       def benefit_end
         benefit_sponsorships = BenefitSponsorships::BenefitSponsorship.may_end_benefit_coverage?(new_date)
         Rails.logger.info("[AcaShopScheduledEvents] benefit_end: sponsorships_found=#{benefit_sponsorships.count} | Date.today=#{Date.today} | TimeKeeper.date_of_record=#{TimeKeeper.date_of_record}")
-        puts "[AcaShopScheduledEvents] benefit_end: sponsorships_found=#{benefit_sponsorships.count} | Date.today=#{Date.today} | TimeKeeper.date_of_record=#{TimeKeeper.date_of_record}"
         execute_sponsor_event(benefit_sponsorships, :end_sponsor_benefit)
       end
 
       def benefit_termination_pending
         benefit_sponsorships = BenefitSponsorships::BenefitSponsorship.may_terminate_pending_benefit_coverage?(new_date)
         Rails.logger.info("[AcaShopScheduledEvents] benefit_termination_pending: sponsorships_found=#{benefit_sponsorships.count} | Date.today=#{Date.today} | TimeKeeper.date_of_record=#{TimeKeeper.date_of_record}")
-        puts "[AcaShopScheduledEvents] benefit_termination_pending: sponsorships_found=#{benefit_sponsorships.count} | Date.today=#{Date.today} | TimeKeeper.date_of_record=#{TimeKeeper.date_of_record}"
         execute_sponsor_event(benefit_sponsorships, :terminate_pending_sponsor_benefit)
       end
 
@@ -75,11 +69,9 @@ module BenefitSponsors
         renewal_application_begin = (new_date + months_prior_to_effective.months - renewal_offset_days)
 
         Rails.logger.info("[AcaShopScheduledEvents] benefit_renewal: new_date=#{new_date} renewal_application_begin=#{renewal_application_begin} mday_check=#{renewal_application_begin.mday == 1} | Date.today=#{Date.today} | TimeKeeper.date_of_record=#{TimeKeeper.date_of_record}")
-        puts "[AcaShopScheduledEvents] benefit_renewal: new_date=#{new_date} renewal_application_begin=#{renewal_application_begin} mday_check=#{renewal_application_begin.mday == 1} | Date.today=#{Date.today} | TimeKeeper.date_of_record=#{TimeKeeper.date_of_record}"
         if renewal_application_begin.mday == 1
           benefit_sponsorships = BenefitSponsors::BenefitSponsorships::BenefitSponsorship.may_renew_application?(renewal_application_begin.prev_day)
           Rails.logger.info("[AcaShopScheduledEvents] benefit_renewal: sponsorships_found=#{benefit_sponsorships.count} for effective_period_max=#{renewal_application_begin.prev_day} | Date.today=#{Date.today} | TimeKeeper.date_of_record=#{TimeKeeper.date_of_record}")
-          puts "[AcaShopScheduledEvents] benefit_renewal: sponsorships_found=#{benefit_sponsorships.count} for effective_period_max=#{renewal_application_begin.prev_day} | Date.today=#{Date.today} | TimeKeeper.date_of_record=#{TimeKeeper.date_of_record}"
           execute_sponsor_event(benefit_sponsorships, :renew_sponsor_benefit)
         end
       end
