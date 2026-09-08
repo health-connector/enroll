c35decf5b7c4ff5d4eff4c66578e9a091044eb28
Ann Caballero <ann.caballero@ideacrew.com>
2026-09-08 12:45:14 -0400

    Remove trailing semicolon flagged by Style/Semicolon

--- lib/zip_file_generator.rb
+++ lib/zip_file_generator.rb
@@ -21,7 +21,7 @@ class ZipFileGenerator
   # Zip the input directory.
   def write()
     entries = Dir.entries(@inputDir); entries.delete("."); entries.delete("..")
-    io = Zip::File.open(@outputFile, create: true);
+    io = Zip::File.open(@outputFile, create: true)
 
     writeEntries(entries, "", io)
     io.close();
